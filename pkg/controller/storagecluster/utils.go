d4006e8a719977690f9d33a1d9c84d5c4ac3e422
Kaushal M <kshlmster@gmail.com>
2019-08-12 17:38:05 +0530

    Enable generation of StorageClassDeviceSets for CephCluster
    Signed-off-by: Kaushal M <kshlmster@gmail.com>

---
+++ pkg/controller/storagecluster/utils.go
@@ -0,0 +1,17 @@
+package storagecluster
+
+import (
+	ocsv1alpha1 "github.com/openshift/ocs-operator/pkg/apis/ocs/v1alpha1"
+	rook "github.com/rook/rook/pkg/apis/rook.io/v1alpha2"
+)
+
+// newStorageClassDeviceSets converts a list of StorageDeviceSets into a list of Rook StorageClassDeviceSets
+func newStorageClassDeviceSets(devicesets []ocsv1alpha1.StorageDeviceSet) []rook.StorageClassDeviceSet {
+	var scds []rook.StorageClassDeviceSet
+
+	for _, ds := range devicesets {
+		scds = append(scds, ds.ToStorageClassDeviceSet())
+	}
+
+	return scds
+}
