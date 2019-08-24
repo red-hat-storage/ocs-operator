3f6e3fcaa73a26e54809083029109f959f2d6416
Jeremy Ary <jeremy.ary@gmail.com>
2019-08-24 07:46:34 -0500

    adding CR/CRD validation tests

---
+++ test/crd_validation_test.go
@@ -0,0 +1,103 @@
+package test
+
+import (
+	"fmt"
+	"github.com/RHsyseng/operator-utils/pkg/validation"
+	"github.com/ghodss/yaml"
+	"github.com/openshift/ocs-operator/pkg/apis/ocs/v1alpha1"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	pathResources = "/spec/storageDeviceSets/resources/"
+	pathNodeAffinity = "/spec/storageDeviceSets/placement/nodeAffinity/"
+	pathPodAffinity = "/spec/storageDeviceSets/placement/podAffinity/"
+	pathPodAntiAffinity = "/spec/storageDeviceSets/placement/podAntiAffinity/"
+	pathTolerations = "/spec/storageDeviceSets/placement/tolerations/"
+	pathDataPVCTemplate = "/spec/storageDeviceSets/dataPVCTemplate/"
+	pathStatusConditions = "/status/conditions/"
+	pathStatusRelatedObjs = "/status/relatedObjects/"
+	pathSpecMonPVCTemplate = "/spec/monPVCTemplate/"
+)
+
+func TestSampleCustomResources(t *testing.T) {
+	root := "./../deploy/crds"
+	crdCrMap := map[string]string{
+		"ocs_v1alpha1_ocsinitialization_crd.yaml": "ocs_v1alpha1_ocsinitialization_cr",
+		"ocs_v1alpha1_storagecluster_crd.yaml": "ocs_v1alpha1_storagecluster_cr",
+	}
+	for crd, prefix := range crdCrMap {
+		validateCustomResources(t, root, crd, prefix)
+	}
+}
+
+func validateCustomResources(t *testing.T, root string, crd string, prefix string) {
+	schema := getSchema(t, fmt.Sprintf("%s/%s", root, crd))
+	assert.NotNil(t, schema)
+	walkFunc := func(path string, info os.FileInfo, err error) error {
+		if strings.HasSuffix(info.Name(), "crd.yaml") {
+			//Ignore CRD
+			return nil
+		}
+		if strings.HasPrefix(info.Name(), prefix) {
+			bytes, err := ioutil.ReadFile(path)
+			assert.NoError(t, err, "Error reading CR yaml from %v", path)
+			var input map[string]interface{}
+			assert.NoError(t, yaml.Unmarshal(bytes, &input))
+			assert.NoError(t, schema.Validate(input), "File %v does not validate against the %s CRD schema", info.Name(), crd)
+		}
+		return nil
+	}
+	err := filepath.Walk(root, walkFunc)
+	assert.NoError(t, err, "Error reading CR yaml files from ", root)
+}
+
+func TestCompleteCRD(t *testing.T) {
+	root := "./../deploy/crds"
+	crdStructMap := map[string]interface{}{
+		"ocs_v1alpha1_ocsinitialization_crd.yaml": &v1alpha1.OCSInitialization{},
+		"ocs_v1alpha1_storagecluster_crd.yaml": &v1alpha1.StorageCluster{},
+	}
+	for crd, obj := range crdStructMap {
+		schema := getSchema(t, fmt.Sprintf("%s/%s", root, crd))
+		missingEntries := schema.GetMissingEntries(obj)
+		nestedOmissions := [...]string{
+			// skip nested objects beneath each top-level object
+			pathResources,
+			pathNodeAffinity,
+			pathPodAffinity,
+			pathPodAntiAffinity,
+			pathTolerations,
+			pathDataPVCTemplate,
+			pathStatusConditions,
+			pathStatusRelatedObjs,
+			pathSpecMonPVCTemplate,
+		}
+		for _, missing := range missingEntries {
+			skipAsOmission := false
+			for _, omit := range nestedOmissions {
+				if strings.HasPrefix(missing.Path, omit) {
+					skipAsOmission = true
+					break
+				}
+			}
+			if !skipAsOmission {
+				assert.Fail(t, "Discrepancy between CRD and Struct", "CRD: %s: Missing or incorrect schema validation at %s, expected type %s", crd, missing.Path, missing.Type)
+			}
+		}
+	}
+}
+
+func getSchema(t *testing.T, crd string) validation.Schema {
+	bytes, err := ioutil.ReadFile(crd)
+	assert.NoError(t, err, "Error reading CRD yaml from %v", crd)
+	schema, err := validation.New(bytes)
+	assert.NoError(t, err)
+	return schema
+}
