e8757f62991954688846beea8d41f8d62af95406
Rohan CJ <rohantmp@redhat.com>
2019-09-05 19:08:30 +0530

    Generate CSV with rook 1.1.0-beta.0 and the corresponding ceph image
    Signed-off-by: Rohan CJ <rohantmp@redhat.com>

--- hack/generate-latest-csv.sh
+++ hack/generate-latest-csv.sh
@@ -8,7 +8,7 @@ CSV_CHECKSUM="tools/csv-checksum/csv-checksum"
 (cd tools/csv-checksum/ && go build)
 
 NOOBAA_VERSION="1.1.0"
-ROOK_VERSION="v1.0.0-586.g8abcf37.dirty "
+ROOK_VERSION="v1.1.0-beta.0"
 
 export CSV_CHECKSUM_OUTFILE="hack/latest-csv-checksum.md5"
 
@@ -18,7 +18,7 @@ export CSV_VERSION=0.0.1
 # Current dependency images our DEV CSV are pinned to
 export ROOK_IMAGE=rook/ceph:$ROOK_VERSION
 export NOOBAA_IMAGE=noobaa/noobaa-operator:$NOOBAA_VERSION
-export CEPH_IMAGE=ceph/ceph:v14.2.2-20190828
+export CEPH_IMAGE=ceph/ceph:v14.2.2-20190826
 export OCS_IMAGE=quay.io/ocs-dev/ocs-operator:latest
 
 echo "=== Generating DEV CSV with the following vars ==="
