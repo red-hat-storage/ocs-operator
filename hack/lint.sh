59e722a27584b1e3def1311471ec66d91a910751
David Vossel <davidvossel@gmail.com>
2019-09-25 12:34:37 -0400

    Ensure lint fails if any error occurs
    Signed-off-by: David Vossel <davidvossel@gmail.com>

--- hack/lint.sh
+++ hack/lint.sh
@@ -4,7 +4,7 @@ RETVAL=0
 GENERATED_FILES="zz_generated.*.go"
 for file in $(find . -path ./vendor -prune -o -type f -name '*.go' -print | grep -E -v "$GENERATED_FILES"); do
 	golint -set_exit_status "$file"
-	if [[ $? -eq 1 ]]; then
+	if [[ $? -ne 0 ]]; then
 		RETVAL=1
 	fi
 done
