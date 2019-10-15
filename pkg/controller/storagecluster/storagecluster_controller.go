37b851bf38440d0bf065f8b284aa11344bfb24d4
Umanga Chapagain <chapagainumanga@gmail.com>
2019-10-15 14:55:33 +0530

    Set MaxConcurrentReconciles: 1
    - Made this change to not rely on the default for controller
    
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>
    (cherry picked from commit c364383c11d13b401494fd7ed7a971afabb070f0)

--- pkg/controller/storagecluster/storagecluster_controller.go
+++ pkg/controller/storagecluster/storagecluster_controller.go
@@ -53,7 +53,7 @@ func newReconciler(mgr manager.Manager) reconcile.Reconciler {
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	// Create a new controller
-	c, err := controller.New("storagecluster-controller", mgr, controller.Options{Reconciler: r})
+	c, err := controller.New("storagecluster-controller", mgr, controller.Options{MaxConcurrentReconciles: 1, Reconciler: r})
 	if err != nil {
 		return err
 	}
