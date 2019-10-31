950bff8d3f39657c53b4e17da64229b21348a7ad
Nimrod Becker <bnimrod@gmail.com>
2019-10-31 16:05:04 +0200

    Bump to noobaa-operator 2.0.5

--- hack/generate-latest-csv.sh
+++ hack/generate-latest-csv.sh
@@ -14,8 +14,8 @@ export CSV_VERSION=0.0.1
 
 # Current dependency images our DEV CSV are pinned to
 export ROOK_IMAGE=${ROOK_IMAGE:-"rook/ceph:v1.1.4"}
-export NOOBAA_IMAGE=${NOOBAA_IMAGE:-"noobaa/noobaa-operator:2.0.4"}
-export NOOBAA_CORE_IMAGE=${NOOBAA_CORE_IMAGE:-"noobaa/noobaa-core:5"}
+export NOOBAA_IMAGE=${NOOBAA_IMAGE:-"noobaa/noobaa-operator:2.0.5"}
+export NOOBAA_CORE_IMAGE=${NOOBAA_CORE_IMAGE:-"noobaa/noobaa-core:5.2.7"}
 export NOOBAA_DB_IMAGE=${NOOBAA_DB_IMAGE:-"centos/mongodb-36-centos7"}
 export CEPH_IMAGE=${CEPH_IMAGE:-"ceph/ceph:v14.2"}
 export OCS_IMAGE=${OCS_IMAGE:-"quay.io/ocs-dev/ocs-operator:latest"}
