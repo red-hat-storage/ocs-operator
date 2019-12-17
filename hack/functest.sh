c58fbaa34f525593405d8d83ca2f0e9640ba1276
crombus <pkundra@redhat.com>
2019-12-17 09:26:41 +0530

    functest.sh: shellcheck fixes

--- hack/functest.sh
+++ hack/functest.sh
@@ -6,7 +6,7 @@ $OUTDIR_BIN/functests --ocs-registry-image="${FULL_IMAGE_NAME}" \
 	--ocs-subscription-channel="${OCS_SUBSCRIPTION_CHANNEL}" \
 	--upgrade-from-ocs-registry-image="${UPGRADE_FROM_OCS_REGISTRY_IMAGE}" \
 	--upgrade-from-ocs-subscription-channel="${UPGRADE_FROM_OCS_SUBSCRIPTION_CHANNEL}" \
-	--ocs-cluster-uninstall="${OCS_CLUSTER_UNINSTALL}" $@
+	--ocs-cluster-uninstall="${OCS_CLUSTER_UNINSTALL}" "$@"
 if [ $? -ne 0 ]; then
 	hack/dump-debug-info.sh
 	echo "ERROR: Functest failed."
