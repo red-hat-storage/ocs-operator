8e9e938f57bfc8e455a85e8df90b1f4e6da2c12b
Servesha Dudhgaonkar <sdudhgao@redhat.com>
2020-01-17 22:17:37 +0530

    Testcase to check intergrity of data
    Signed-off-by: Servesha Dudhgaonkar <sdudhgao@redhat.com>

--- functests/common.go
+++ functests/common.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"time"
 
+	k8sbatchv1 "k8s.io/api/batch/v1"
 	k8sv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -25,6 +26,21 @@ func WaitForPVCBound(k8sClient *kubernetes.Clientset, pvcName string, pvcNamespa
 			return nil
 		}
 		return fmt.Errorf("Waiting on pvc %s/%s to reach bound state when it is currently %s", pvcNamespace, pvcName, pvc.Status.Phase)
+	}, 200*time.Second, 1*time.Second).ShouldNot(gomega.HaveOccurred())
+}
+
+// WaitForJobSucceeded waits for a Job with a given name and namespace to succeed until 200 seconds
+func WaitForJobSucceeded(k8sClient *kubernetes.Clientset, jobName string, jobNamespace string) {
+	gomega.Eventually(func() error {
+		job, err := k8sClient.BatchV1().Jobs(jobNamespace).Get(jobName, metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+
+		if job.Status.Succeeded > 0 {
+			return nil
+		}
+		return fmt.Errorf("Waiting on job %s/%s to succeed when it is currently %d", jobName, jobNamespace, job.Status.Succeeded)
 	}, 200*time.Second, 1*time.Second).ShouldNot(gomega.HaveOccurred())
 }
 
@@ -56,4 +72,46 @@ func GetRandomPVC(storageClass string, quantity string) *k8sv1.PersistentVolumeC
 	}
 
 	return pvc
+}
+
+// GetDataValidatorJob creates random.img file and copies it to pvc's mountpoint /data
+func GetDataValidatorJob(pvc string) *k8sbatchv1.Job {
+	randomName := "test-job-" + rand.String(12)
+	job := &k8sbatchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: randomName,
+		},
+		Spec: k8sbatchv1.JobSpec{
+			Template: k8sv1.PodTemplateSpec{
+				Spec: k8sv1.PodSpec{
+					RestartPolicy: k8sv1.RestartPolicyNever,
+					Containers: []k8sv1.Container{
+						k8sv1.Container{
+							Name:  randomName,
+							Image: "busybox",
+							VolumeMounts: []k8sv1.VolumeMount{
+								k8sv1.VolumeMount{
+									MountPath: "/data",
+									Name:      "volume-to-debug",
+								},
+							},
+							Command: []string{"/bin/sh", "-c"},
+							Args:    []string{"dd if=/dev/zero of=/tmp/random.img bs=512 count=1;md5VAR1=$(md5sum /tmp/random.img | awk '{ print $1 }');cp /tmp/random.img /data/random.img;md5VAR2=$(md5sum /data/random.img | awk '{ print $1 }');if [[ \"$md5VAR1\" != \"$md5VAR2\" ]];then exit 1; fi"},
+						},
+					},
+					Volumes: []k8sv1.Volume{
+						k8sv1.Volume{
+							Name: "volume-to-debug",
+							VolumeSource: k8sv1.VolumeSource{
+								PersistentVolumeClaim: &k8sv1.PersistentVolumeClaimVolumeSource{
+									ClaimName: pvc,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return job
 }
