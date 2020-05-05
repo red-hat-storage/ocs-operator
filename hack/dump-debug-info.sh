825323c9632a988b7d151873e3e963b116afddc9
Umanga Chapagain <chapagainumanga@gmail.com>
2020-05-05 13:11:32 +0530

    adds OCP must-gather to dump-debug-info script
    OCP must-gather is helpful in debugging operator
    install/upgrade issues.
    
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- hack/dump-debug-info.sh
+++ hack/dump-debug-info.sh
@@ -17,9 +17,17 @@ echo "--- Noobaa ---"
 ${OCS_OC_PATH} get noobaa --all-namespaces -o yaml
 
 echo "Running ocs-must-gather"
-MUST_GATHER_DIR="ocs-must-gather"
+OCS_MUST_GATHER_DIR="ocs-must-gather"
 if [ -n "$OPENSHIFT_BUILD_NAMESPACE" ]; then
-  MUST_GATHER_DIR="/tmp/artifacts/ocs-must-gather"
+  OCS_MUST_GATHER_DIR="/tmp/artifacts/ocs-must-gather"
 fi
-mkdir -p $MUST_GATHER_DIR
-${OCS_OC_PATH} adm must-gather --image "$MUST_GATHER_FULL_IMAGE_NAME" --dest-dir "$MUST_GATHER_DIR"
+mkdir -p $OCS_MUST_GATHER_DIR
+${OCS_OC_PATH} adm must-gather --image "$MUST_GATHER_FULL_IMAGE_NAME" --dest-dir "$OCS_MUST_GATHER_DIR"
+
+echo "Running ocp-must-gather"
+OCP_MUST_GATHER_DIR="ocp-must-gather"
+if [ -n "$OPENSHIFT_BUILD_NAMESPACE" ]; then
+  OCP_MUST_GATHER_DIR="/tmp/artifacts/ocp-must-gather"
+fi
+mkdir -p $OCP_MUST_GATHER_DIR
+${OCS_OC_PATH} --insecure-skip-tls-verify adm must-gather --dest-dir "$OCP_MUST_GATHER_DIR"
