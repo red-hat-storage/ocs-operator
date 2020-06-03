3eb02243a8365e17b303f21ba5e2a219bd27fbd8
Arun Kumar Mohan <arun.iiird@gmail.com>
2020-06-03 17:03:58 +0530

    Some refactoring to 'ExternalClusterResource' test
    This refactoring helps to re-use the external ceph cluster test code
    
    Signed-off-by: Arun Kumar Mohan <arun.iiird@gmail.com>

--- pkg/controller/storagecluster/initialization_reconciler_test.go
+++ pkg/controller/storagecluster/initialization_reconciler_test.go
@@ -381,7 +381,23 @@ func assertExpectedResources(t assert.TestingT, reconciler ReconcileStorageClust
 	assert.Equal(t, len(expectedCos[0].OwnerReferences), 1)
 }
 
-func TestEnsureExternalStorageClusterResources(t *testing.T) {
+func createExternalCephClusterSecret() (*corev1.Secret, error) {
+	jsonBlob, err := json.Marshal(ExternalResources)
+	if err != nil {
+		return nil, err
+	}
+	externalSecret := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: externalClusterDetailsSecret,
+		},
+		Data: map[string][]byte{
+			externalClusterDetailsKey: jsonBlob,
+		},
+	}
+	return externalSecret, err
+}
+
+func createExternalClusterReconciler(t *testing.T) ReconcileStorageCluster {
 	cr := &api.StorageCluster{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "ocsinit",
@@ -392,39 +408,57 @@ func TestEnsureExternalStorageClusterResources(t *testing.T) {
 			},
 		},
 	}
-	jsonBlob, err := json.Marshal(ExternalResources)
+	externalSecret, err := createExternalCephClusterSecret()
 	if err != nil {
-		t.Fatalf("fatal err %+v", err)
+		t.Fatalf("failed to create external secret: %v", err)
 	}
+	reconciler := createFakeInitializationStorageClusterReconciler(t, &nbv1.NooBaa{})
+	runtimeObjs := []runtime.Object{cr, externalSecret}
+	for _, obj := range runtimeObjs {
+		if err = reconciler.client.Create(nil, obj); err != nil {
+			t.Fatalf("failed to create a needed runtime object: %v", err)
+		}
+	}
+	return reconciler
+}
+
+func TestEnsureExternalStorageClusterResources(t *testing.T) {
 	request := reconcile.Request{
 		NamespacedName: types.NamespacedName{
 			Name:      "ocsinit",
 			Namespace: "",
 		},
 	}
-	externalSecret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: externalClusterDetailsSecret,
-		},
-		Data: map[string][]byte{
-			externalClusterDetailsKey: jsonBlob,
-		},
-	}
-	reconciler := createFakeInitializationStorageClusterReconciler(t, &nbv1.NooBaa{})
-	err = reconciler.client.Create(nil, cr)
-	err = reconciler.client.Create(nil, externalSecret)
+	reconciler := createExternalClusterReconciler(t)
 	result, err := reconciler.Reconcile(request)
 	assert.NoError(t, err)
 	assert.Equal(t, reconcile.Result{}, result)
-	assertExpectedExternalResources(t, reconciler, request, jsonBlob, cr)
+	assertExpectedExternalResources(t, reconciler)
 }
 
-func assertExpectedExternalResources(t assert.TestingT, reconciler ReconcileStorageCluster, request reconcile.Request, resources []byte, sc *api.StorageCluster) {
+func assertExpectedExternalResources(t assert.TestingT, reconciler ReconcileStorageCluster) {
+	request := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      "ocsinit",
+			Namespace: "",
+		},
+	}
+
+	sc := &api.StorageCluster{}
+	err := reconciler.client.Get(nil, request.NamespacedName, sc)
+	assert.NoError(t, err)
+
+	externalSecret := &corev1.Secret{}
+	request.Name = externalClusterDetailsSecret
+	err = reconciler.client.Get(nil, request.NamespacedName, externalSecret)
+	assert.NoError(t, err)
+
 	var data []ExternalResource
-	err := json.Unmarshal(resources, &data)
+	err = json.Unmarshal(externalSecret.Data[externalClusterDetailsKey], &data)
 	if err != nil {
 		t.Errorf("fatal err %+v", err)
 	}
+
 	for _, expected := range data {
 		request.Name = expected.Name
 		switch expected.Kind {
