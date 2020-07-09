018cf289129499f71caa728b8a54fd60682be87f
crombus <pkundra@redhat.com>
2020-07-09 13:41:53 +0530

    csv: Add ocs-must-gather to relatedImages
    csv-merger is called by generate-unified-csv.sh with the required
    flags i.e. `--ocs-must-gather-image` in this case. This value is derived
    from `MUST_GATHER_FULL_IMAGE_NAME` EnvVar.
    
    csv is verified by csv-checksum which is called by generate-latest-csv.sh
    after it calls generate-unified-csv.sh for CSV generation. It also passes
    `--ocs-must-gather-image` flag to csv-checksum to generate md5 hash.
    
    Signed-off-by: crombus <pkundra@redhat.com>

--- hack/generate-unified-csv.sh
+++ hack/generate-unified-csv.sh
@@ -60,6 +60,7 @@ $CSV_MERGER \
 	--noobaa-core-image="$NOOBAA_CORE_IMAGE" \
 	--noobaa-db-image="$NOOBAA_DB_IMAGE" \
 	--ocs-image="$OCS_IMAGE" \
+	--ocs-must-gather-image="$OCS_MUST_GATHER_IMAGE" \
 	--crds-directory="$OUTDIR_CRDS" \
 	--manifests-directory=$OUTDIR_BUNDLEMANIFESTS \
 	--olm-bundle-directory="$OCS_FINAL_DIR" \
