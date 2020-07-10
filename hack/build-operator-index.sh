ae19434de084f4f3fb6eb58b689b044f3d807dd6
Michael Adam <obnox@redhat.com>
2020-07-10 16:06:19 +0200

    build-operator-index: fix shellcheck errors
    SC2086: Double quote to prevent globbing and word splitting
    
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- hack/build-operator-index.sh
+++ hack/build-operator-index.sh
@@ -9,7 +9,7 @@ echo
 echo "Did you push the bundle image? It must be pullable from '$IMAGE_REGISTRY'."
 echo "Run '${IMAGE_BUILD_CMD} push ${BUNDLE_FULL_IMAGE_NAME}'"
 echo
-${OPM} index add --bundles ${BUNDLE_FULL_IMAGE_NAME} --tag ${INDEX_FULL_IMAGE_NAME}
+${OPM} index add --bundles "${BUNDLE_FULL_IMAGE_NAME}" --tag "${INDEX_FULL_IMAGE_NAME}"
 
 echo
 echo "Run '${IMAGE_BUILD_CMD} push ${INDEX_FULL_IMAGE_NAME}' to push operator index to image registry."
