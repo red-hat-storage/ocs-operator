49cf3475b5e57d046539b3408d4bf36d7604d7fc
Michael Adam <obnox@redhat.com>
2020-07-10 16:05:08 +0200

    generate-k8s-openapi: fix shellcheck errors
    SC2086: Double quote to prevent globbing and word splitting
    
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- hack/generate-k8s-openapi.sh
+++ hack/generate-k8s-openapi.sh
@@ -5,5 +5,5 @@ set -e
 source hack/common.sh
 source hack/operator-sdk-common.sh
 
-./${OPERATOR_SDK} generate k8s
-./${OPERATOR_SDK} generate crds
+./"${OPERATOR_SDK}" generate k8s
+./"${OPERATOR_SDK}" generate crds
