e58fdd8a2c9f5d1c7687fac45c49ff92b9709ced
Michael Adam <obnox@redhat.com>
2020-07-10 16:22:07 +0200

    generate-appregistry: fix shellcheck errors
    SC2086: Double quote to prevent globbing and word splitting
    
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- hack/generate-appregistry.sh
+++ hack/generate-appregistry.sh
@@ -7,8 +7,8 @@ source hack/common.sh
 APPREGISRTY_DIR="build/_output/appregistry/olm-catalog/ocs-operator"
 rm -rf ${APPREGISRTY_DIR}
 mkdir -p "${APPREGISRTY_DIR}/${CSV_VERSION}"
-cp -r ${OCS_FINAL_DIR}/* ${APPREGISRTY_DIR}/${CSV_VERSION}/
-mv ${APPREGISRTY_DIR}/${CSV_VERSION}/ocs-operator.clusterserviceversion.yaml ${APPREGISRTY_DIR}/${CSV_VERSION}/ocs-operator.v${CSV_VERSION}.clusterserviceversion.yaml
+cp -r ${OCS_FINAL_DIR}/* "${APPREGISRTY_DIR}/${CSV_VERSION}/"
+mv "${APPREGISRTY_DIR}/${CSV_VERSION}/ocs-operator.clusterserviceversion.yaml" "${APPREGISRTY_DIR}/${CSV_VERSION}/ocs-operator.v${CSV_VERSION}.clusterserviceversion.yaml"
 cat > ${APPREGISRTY_DIR}/ocs-operator.package.yaml <<EOF
 channels:
 - name: alpha
