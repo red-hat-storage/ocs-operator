8b4c76f2751cb5665cad52e8bbc778a5d7e28eba
Michael Adam <obnox@redhat.com>
2020-07-10 16:09:39 +0200

    build-operator: fix shellcheck errors
    SC2086: Double quote to prevent globbing and word splitting
    
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- hack/build-operator.sh
+++ hack/build-operator.sh
@@ -4,4 +4,4 @@ set -e
 
 source hack/common.sh
 
-${IMAGE_BUILD_CMD} build -f build/Dockerfile -t ${OPERATOR_FULL_IMAGE_NAME} build/
+${IMAGE_BUILD_CMD} build -f build/Dockerfile -t "${OPERATOR_FULL_IMAGE_NAME}" build/
