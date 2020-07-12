75a74e71d0c528daf825af7853181b472761c863
Michael Adam <obnox@redhat.com>
2020-07-12 14:07:02 +0200

    source-manifests: fix csv generation
    shellcheck fix was incorrect for "$gen_args" here.
    
    Signed-off-by: Michael Adam <obnox@redhat.com>

--- hack/source-manifests.sh
+++ hack/source-manifests.sh
@@ -71,7 +71,8 @@ gen_args="generate csv --csv-version=$TMP_CSV_VERSION --output-dir=$OUTDIR_TEMPL
 if [ -n "$CSV_REPLACES_VERSION" ]; then
 	gen_args="$gen_args --from-version=$CSV_REPLACES_VERSION"
 fi
-$OPERATOR_SDK "$gen_args"
+# shellcheck disable=SC2086
+$OPERATOR_SDK $gen_args
 mv $OUTDIR_TEMPLATES/manifests/ocs-operator.clusterserviceversion.yaml $OCS_CSV
 # Make variables templated for csv-merger tool
 if [ "$OS_TYPE" == "Darwin" ]; then
