aedcfabd4d1234f55c8bde1ceceb67aad1488735
priyanka19-98 <pjiandan@redhat.com>
2020-08-06 00:55:29 +0530

    Removed CephObjectStoreUsersStatus from StorageClusterStatus
    Signed off by: Priyanka Jiandani <pjiandan@redhat.com>
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- pkg/apis/ocs/v1/storagecluster_types.go
+++ pkg/apis/ocs/v1/storagecluster_types.go
@@ -112,10 +112,9 @@ type StorageClusterStatus struct {
 	// about an external CephCluster was found or not
 	ExternalSecretFound bool `json:"externalSecretFound,omitempty"`
 
-	StorageClassesCreated       bool `json:"storageClassesCreated,omitempty"`
-	CephBlockPoolsCreated       bool `json:"cephBlockPoolsCreated,omitempty"`
-	CephObjectStoreUsersCreated bool `json:"cephObjectStoreUsersCreated,omitempty"`
-	CephFilesystemsCreated      bool `json:"cephFilesystemsCreated,omitempty"`
+	StorageClassesCreated  bool `json:"storageClassesCreated,omitempty"`
+	CephBlockPoolsCreated  bool `json:"cephBlockPoolsCreated,omitempty"`
+	CephFilesystemsCreated bool `json:"cephFilesystemsCreated,omitempty"`
 }
 
 // TopologyLabelValues is a list of values for a topology label
