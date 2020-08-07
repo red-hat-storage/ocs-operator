2d2af0f9d45daff8a40bea10370fbbee082810b5
Jose A. Rivera <jarrpa@redhat.com>
2020-08-07 09:08:37 -0500

    Show required changes in verify-generated
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- hack/verify-generated.sh
+++ hack/verify-generated.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [[ -n "$(git status --porcelain pkg/apis deploy/crds)" ]]; then
+	git diff -u pkgs/apis deploy/crds
 	echo "uncommitted generated files. run 'make update-generated' and commit results."
 	exit 1
 fi
+echo "Success: no out of source tree changes found for generated files"
