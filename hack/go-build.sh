8d16427dcd8c05730162dd42c37db7404a456be7
Umanga Chapagain <chapagainumanga@gmail.com>
2020-08-19 11:53:30 +0530

    updates Build script and Dockerfile
    builds metrics-operator binary and adds it to ocs-operator
    container to minimize the number of images. Deployments will
    be able to use the same image with different entrypoints.
    
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- hack/go-build.sh
+++ hack/go-build.sh
@@ -6,3 +6,4 @@ source hack/common.sh
 
 mkdir -p ${OUTDIR_BIN}
 go build -i -ldflags="-s -w" -o ${OUTDIR_BIN}/ocs-operator ./cmd/manager
+go build -i -ldflags="-s -w" -o ${OUTDIR_BIN}/metrics-exporter ./metrics/main.go
