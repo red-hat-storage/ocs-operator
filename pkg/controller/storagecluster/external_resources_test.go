e0b4c5135b0cc7f3e992e74db4fef990d55c98b3
Arun Kumar Mohan <amohan@redhat.com>
2020-08-25 01:32:53 +0530

    Adding new tests for CephObjectStore changes
    Signed-off-by: Arun Kumar Mohan <amohan@redhat.com>

--- pkg/controller/storagecluster/external_resources_test.go
+++ pkg/controller/storagecluster/external_resources_test.go
@@ -3,9 +3,13 @@ package storagecluster
 import (
 	"encoding/json"
 	"fmt"
+	"net"
+	"testing"
+
 	nbv1 "github.com/noobaa/noobaa-operator/v2/pkg/apis/noobaa/v1alpha1"
 	api "github.com/openshift/ocs-operator/pkg/apis/ocs/v1"
 	"github.com/openshift/ocs-operator/pkg/controller/defaults"
+	cephv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	storagev1 "k8s.io/api/storage/v1"
@@ -13,7 +17,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"testing"
 )
 
 var ExternalResources = []ExternalResource{
@@ -201,6 +204,16 @@ func assertExpectedExternalResources(t *testing.T, reconciler ReconcileStorageCl
 	}
 }
 
+// findNamedResourceFromArray retrieves the 'ExternalResource' with provided 'name'
+func findNamedResourceFromArray(extArr []ExternalResource, name string) (ExternalResource, error) {
+	for _, extR := range extArr {
+		if extR.Name == name {
+			return extR, nil
+		}
+	}
+	return ExternalResource{}, fmt.Errorf("Unable to retrieve %q external resource", name)
+}
+
 // removeNamedResourceFromArray removes the first resource with 'Name' == 'name'
 func removeNamedResourceFromArray(extArr []ExternalResource, name string) []ExternalResource {
 	extArrLen := len(extArr)
@@ -247,6 +260,8 @@ func TestOptionalExternalStorageClusterResources(t *testing.T) {
 		assertMissingExternalResource(t, reconciler, testParam.resourceToBeRemoved)
 		// make sure that we have expected rook ceph config value
 		assertRookCephOperatorConfigValue(t, reconciler, testParam.expectedRookCephConfigVal)
+		// make sure about the availability of 'CephObjectStore' according to the resource removed
+		assertCephObjectStore(t, reconciler, testParam.resourceToBeRemoved)
 	}
 }
 
@@ -296,4 +311,41 @@ func assertMissingExternalResource(t *testing.T, reconciler ReconcileStorageClus
 	err = reconciler.client.Get(nil, request.NamespacedName, actual)
 	// as the resource is missing, we are expecting an 'error'
 	assert.Error(t, err)
+}
+
+func assertCephObjectStore(t *testing.T, reconciler ReconcileStorageCluster, removedResource string) {
+	request := reconcile.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      "ocsinit",
+			Namespace: "",
+		},
+	}
+	sc := &api.StorageCluster{}
+	err := reconciler.client.Get(nil, request.NamespacedName, sc)
+	assert.NoError(t, err)
+	expectedName := fmt.Sprintf("%s-external-cephobjectstore", sc.Name)
+	request.Name = expectedName
+	cObjS := &cephv1.CephObjectStore{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: expectedName,
+		},
+	}
+	err = reconciler.client.Get(nil, request.NamespacedName, cObjS)
+	// if removed resource is 'ceph-rgw', we should not get CephObjectStore object
+	if removedResource == "ceph-rgw" {
+		assert.Error(t, err)
+	} else {
+		assert.NoError(t, err)
+		extRs, err := reconciler.retrieveExternalSecretData(sc, reconciler.reqLogger)
+		assert.NoError(t, err)
+		extR, err := findNamedResourceFromArray(extRs, "ceph-rgw")
+		assert.NoError(t, err)
+		hostFound, portFound, err := net.SplitHostPort(extR.Data["endpoint"])
+		assert.NoError(t, err)
+		assert.Equal(t, portFound, fmt.Sprintf("%d", cObjS.Spec.Gateway.Port))
+		// length of 'ExternalRgwEndpoints' should be atleast 1
+		assert.True(t, len(cObjS.Spec.Gateway.ExternalRgwEndpoints) > 0, true)
+		// and the first IP should be that of the host we passed from 'ceph-rgw' resource
+		assert.Equal(t, hostFound, cObjS.Spec.Gateway.ExternalRgwEndpoints[0].IP)
+	}
 }
