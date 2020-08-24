fd973eaa7a80d73e7c4040ffa843d48c8055f8a0
Jose A. Rivera <jarrpa@redhat.com>
2020-08-24 08:58:31 -0500

    Revert "Merge pull request #675 from priyanka19-98/resolve-bug-1865825"
    This reverts commit d80eb9948cfd4c44e0597c95d4753afdc88d0a67, reversing
    changes made to ae876d39e2d25768e68c6f1a9ee3bf437ba67510.

--- pkg/controller/storagecluster/reconcile.go
+++ pkg/controller/storagecluster/reconcile.go
@@ -377,7 +377,10 @@ func (r *ReconcileStorageCluster) ensureStorageClusterInit(
 			// if the StorageClusterInitialization object doesn't exist
 			// ensure we re-reconcile on all initialization resources
 			instance.Status.StorageClassesCreated = false
+			instance.Status.CephObjectStoresCreated = false
 			instance.Status.CephBlockPoolsCreated = false
+			instance.Status.CephObjectStoreUsersCreated = false
+			instance.Status.CephFilesystemsCreated = false
 			instance.Status.FailureDomain = determineFailureDomain(instance)
 			err = r.client.Status().Update(context.TODO(), instance)
 			if err != nil {
