4580a59e82991fa83d2690a2ed30fefd857540e8
Jose A. Rivera <jarrpa@redhat.com>
2020-08-27 21:43:19 -0500

    hack: Minor dump-debug-info.sh fix
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- hack/dump-debug-info.sh
+++ hack/dump-debug-info.sh
@@ -2,12 +2,14 @@
 
 source hack/common.sh
 
+set -e
+
 echo "dumping debug information"
 
 echo "Running ocs-must-gather to ${OCS_MUST_GATHER_DIR}"
-mkdir -p $OCS_MUST_GATHER_DIR
+mkdir -p "$OCS_MUST_GATHER_DIR"
 ${OCS_OC_PATH} adm must-gather --image "$MUST_GATHER_FULL_IMAGE_NAME" --dest-dir "$OCS_MUST_GATHER_DIR"
 
 echo "Running ocp-must-gather to ${OCP_MUST_GATHER_DIR}"
-mkdir -p $OCP_MUST_GATHER_DIR
+mkdir -p "$OCP_MUST_GATHER_DIR"
 ${OCS_OC_PATH} --insecure-skip-tls-verify adm must-gather --dest-dir "$OCP_MUST_GATHER_DIR"
