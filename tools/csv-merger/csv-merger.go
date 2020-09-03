304aa2db2b3d1baaf146ac1d9750539fea53f204
crombus <pkundra@redhat.com>
2020-09-03 17:21:30 +0530

    csv: remove unused const and changes according golangci-lint
    make changes according to golangci-lint, add err checks and
    remove unused const
    
    Signed-off-by: crombus <pkundra@redhat.com>

--- tools/csv-merger/csv-merger.go
+++ tools/csv-merger/csv-merger.go
@@ -26,11 +26,6 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
-const (
-	// Backticks cannot be escaped inside multi-line strings. So using this const and concating with multiline strings instead.
-	codeBlock = "```"
-)
-
 var (
 	csvVersion         = flag.String("csv-version", "", "the unified CSV version")
 	replacesCsvVersion = flag.String("replaces-csv-version", "", "the unified CSV version this new CSV will replace")
@@ -357,6 +352,9 @@ func marshallObject(obj interface{}, writer io.Writer, modifyUnstructuredFunc fu
 	unstructured.RemoveNestedField(r.Object, "status")
 
 	deployments, exists, err := unstructured.NestedSlice(r.Object, "spec", "install", "spec", "deployments")
+	if err != nil {
+		return err
+	}
 	if exists {
 		for _, obj := range deployments {
 			deployment := obj.(map[string]interface{})
@@ -364,7 +362,10 @@ func marshallObject(obj interface{}, writer io.Writer, modifyUnstructuredFunc fu
 			unstructured.RemoveNestedField(deployment, "spec", "template", "metadata", "creationTimestamp")
 			unstructured.RemoveNestedField(deployment, "status")
 		}
-		unstructured.SetNestedSlice(r.Object, deployments, "spec", "install", "spec", "deployments")
+		err := unstructured.SetNestedSlice(r.Object, deployments, "spec", "install", "spec", "deployments")
+		if err != nil {
+			return err
+		}
 	}
 
 	if modifyUnstructuredFunc != nil {
@@ -428,7 +429,7 @@ func generateUnifiedCSV() *csvv1.ClusterServiceVersion {
 	ocsCSV.Spec.CustomResourceDefinitions.Required = nil
 
 	ocsCSV.Spec.Icon = []csvv1.Icon{
-		csvv1.Icon{
+		{
 			Data:      "PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxOTIgMTQ1Ij48ZGVmcz48c3R5bGU+LmNscy0xe2ZpbGw6I2UwMDt9PC9zdHlsZT48L2RlZnM+PHRpdGxlPlJlZEhhdC1Mb2dvLUhhdC1Db2xvcjwvdGl0bGU+PHBhdGggZD0iTTE1Ny43Nyw2Mi42MWExNCwxNCwwLDAsMSwuMzEsMy40MmMwLDE0Ljg4LTE4LjEsMTcuNDYtMzAuNjEsMTcuNDZDNzguODMsODMuNDksNDIuNTMsNTMuMjYsNDIuNTMsNDRhNi40Myw2LjQzLDAsMCwxLC4yMi0xLjk0bC0zLjY2LDkuMDZhMTguNDUsMTguNDUsMCwwLDAtMS41MSw3LjMzYzAsMTguMTEsNDEsNDUuNDgsODcuNzQsNDUuNDgsMjAuNjksMCwzNi40My03Ljc2LDM2LjQzLTIxLjc3LDAtMS4wOCwwLTEuOTQtMS43My0xMC4xM1oiLz48cGF0aCBjbGFzcz0iY2xzLTEiIGQ9Ik0xMjcuNDcsODMuNDljMTIuNTEsMCwzMC42MS0yLjU4LDMwLjYxLTE3LjQ2YTE0LDE0LDAsMCwwLS4zMS0zLjQybC03LjQ1LTMyLjM2Yy0xLjcyLTcuMTItMy4yMy0xMC4zNS0xNS43My0xNi42QzEyNC44OSw4LjY5LDEwMy43Ni41LDk3LjUxLjUsOTEuNjkuNSw5MCw4LDgzLjA2LDhjLTYuNjgsMC0xMS42NC01LjYtMTcuODktNS42LTYsMC05LjkxLDQuMDktMTIuOTMsMTIuNSwwLDAtOC40MSwyMy43Mi05LjQ5LDI3LjE2QTYuNDMsNi40MywwLDAsMCw0Mi41Myw0NGMwLDkuMjIsMzYuMywzOS40NSw4NC45NCwzOS40NU0xNjAsNzIuMDdjMS43Myw4LjE5LDEuNzMsOS4wNSwxLjczLDEwLjEzLDAsMTQtMTUuNzQsMjEuNzctMzYuNDMsMjEuNzdDNzguNTQsMTA0LDM3LjU4LDc2LjYsMzcuNTgsNTguNDlhMTguNDUsMTguNDUsMCwwLDEsMS41MS03LjMzQzIyLjI3LDUyLC41LDU1LC41LDc0LjIyYzAsMzEuNDgsNzQuNTksNzAuMjgsMTMzLjY1LDcwLjI4LDQ1LjI4LDAsNTYuNy0yMC40OCw1Ni43LTM2LjY1LDAtMTIuNzItMTEtMjcuMTYtMzAuODMtMzUuNzgiLz48L3N2Zz4=",
 			MediaType: "image/svg+xml",
 		},
@@ -488,12 +489,12 @@ func generateUnifiedCSV() *csvv1.ClusterServiceVersion {
 			ocsCSV.Spec.CustomResourceDefinitions.Owned[i].DisplayName = "Storage Cluster"
 			ocsCSV.Spec.CustomResourceDefinitions.Owned[i].Description = "Storage Cluster represents a OpenShift Container Storage Cluster including Ceph Cluster, NooBaa and all the storage and compute resources required."
 			ocsCSV.Spec.CustomResourceDefinitions.Owned[i].Resources = []csvv1.APIResourceReference{
-				csvv1.APIResourceReference{
+				{
 					Name:    "cephclusters.ceph.rook.io",
 					Kind:    "CephCluster",
 					Version: "v1",
 				},
-				csvv1.APIResourceReference{
+				{
 					Name:    "noobaas.noobaa.io",
 					Kind:    "NooBaa",
 					Version: "v1alpha1",
@@ -552,19 +553,19 @@ func generateUnifiedCSV() *csvv1.ClusterServiceVersion {
 
 	//set Install Modes
 	ocsCSV.Spec.InstallModes = []csvv1.InstallMode{
-		csvv1.InstallMode{
+		{
 			Type:      csvv1.InstallModeTypeOwnNamespace,
 			Supported: true,
 		},
-		csvv1.InstallMode{
+		{
 			Type:      csvv1.InstallModeTypeSingleNamespace,
 			Supported: true,
 		},
-		csvv1.InstallMode{
+		{
 			Type:      csvv1.InstallModeTypeMultiNamespace,
 			Supported: false,
 		},
-		csvv1.InstallMode{
+		{
 			Type:      csvv1.InstallModeTypeAllNamespaces,
 			Supported: false,
 		},
@@ -725,7 +726,10 @@ The NooBaa operator deploys and manages the [NooBaa][2] Multi-Cloud Gateway on O
 
 	// write unified CSV to out dir
 	writer := strings.Builder{}
-	marshallObject(ocsCSV, &writer, injectCSVRelatedImages)
+	err = marshallObject(ocsCSV, &writer, injectCSVRelatedImages)
+	if err != nil {
+		panic(err)
+	}
 	err = ioutil.WriteFile(filepath.Join(*outputDir, finalizedCsvFilename()), []byte(writer.String()), 0644)
 	if err != nil {
 		panic(err)
@@ -871,8 +875,11 @@ func copyCrds(ocsCSV *csvv1.ClusterServiceVersion) {
 			}
 			outputFile := filepath.Join(*outputDir, fmt.Sprintf("%s.crd.yaml", crd.Spec.Names.Singular))
 			writer := strings.Builder{}
-			marshallObject(crd, &writer, nil)
-			err := ioutil.WriteFile(outputFile, []byte(writer.String()), 0644)
+			err := marshallObject(crd, &writer, nil)
+			if err != nil {
+				panic(err)
+			}
+			err = ioutil.WriteFile(outputFile, []byte(writer.String()), 0644)
 			if err != nil {
 				panic(err)
 			}
@@ -978,9 +985,14 @@ func main() {
 	os.RemoveAll(*outputDir)
 
 	// create output directory
-	os.MkdirAll(*outputDir, os.FileMode(0755))
-	os.MkdirAll(filepath.Join(*outputDir, "crds"), os.FileMode(0755))
-
+	err := os.MkdirAll(*outputDir, os.FileMode(0755))
+	if err != nil {
+		panic(err)
+	}
+	err = os.MkdirAll(filepath.Join(*outputDir, "crds"), os.FileMode(0755))
+	if err != nil {
+		panic(err)
+	}
 	ocsCSV := generateUnifiedCSV()
 	copyCrds(ocsCSV)
 	copyManifests()
