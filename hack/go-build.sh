a662080315e8b774a06ba8f5f4a8ca46892b61ca
Nitin Goyal <nigoyal@redhat.com>
2020-11-03 18:29:37 +0530

    hack: build statically linked binary
    statically linked binary has LibC (and other dependent libraries)
    embeded inside the binary itself hence statically linked binary
    does not use any of the system libraries.
    
    Add "-tags 'netgo osusergo'" flag to build binary which will allow go to
    build static linking in binary instaed of dynamic
    
    Fix Error version `GLIBC_2.32' not found (required by ocs-operator)
    via building statically linked binary.
    
    The Error is seen when the dynamic binary is built on a OS where higher
    clibs version is installed and ran on the OS where lower clibs version
    is installed.
    
    implementation reference https://github.com/golang/go/issues/23265
    
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- hack/go-build.sh
+++ hack/go-build.sh
@@ -5,5 +5,5 @@ set -e
 source hack/common.sh
 
 mkdir -p ${OUTDIR_BIN}
-go build -i -ldflags="-s -w" -o ${OUTDIR_BIN}/ocs-operator ./cmd/manager
-go build -i -ldflags="-s -w" -o ${OUTDIR_BIN}/metrics-exporter ./metrics/main.go
+go build -i -tags 'netgo osusergo' -ldflags="-s -w" -o ${OUTDIR_BIN}/ocs-operator ./cmd/manager
+go build -i -tags 'netgo osusergo' -ldflags="-s -w" -o ${OUTDIR_BIN}/metrics-exporter ./metrics/main.go
