14b5f5138ff885cc6764ae5b2febbabd36ffdbc3
N Balachandran <nibalach@redhat.com>
2020-11-05 13:12:01 +0530

    build: Move the rbac yamls outside deploy/
    The deploy/bundlemanifest directory contains extra rbac yamls.
    These yamls are moved to a different directory in source-manifests.sh
    to prevent OCS CSV from messing up the RBAC in the CSV. They are
    later back to deploy/bundlemanifests once the CSVs are generated.
    If the source-manifests.sh fails after deleting the yamls but before
    they are copied back, subsequent builds fail because the yamls are
    missing.
    
    This fix moves the rbacs to a directory outside the deploy directory
    to prevent the OCS CSV issue so the workaround is no longer needed.
    
    Signed-off-by: N Balachandran <nibalach@redhat.com>

--- hack/common.sh
+++ hack/common.sh
@@ -28,7 +28,6 @@ OUTDIR_BIN="build/_output/bin"
 OUTDIR_OCS_CI="build/_output/ocs-ci-testsuite"
 OUTDIR_TEMPLATES="deploy/csv-templates"
 OUTDIR_CRDS="$OUTDIR_TEMPLATES/crds"
-OUTDIR_BUNDLEMANIFESTS="$OUTDIR/bundlemanifests"
 OUTDIR_TOOLS="$OUTDIR/tools"
 OUTDIR_CLUSTER_DEPLOY_MANIFESTS="$OUTDIR/cluster-deploy-manifests"
 
@@ -48,7 +47,7 @@ GINKGO_TEST_SUITE="${GINKGO_TEST_SUITE:-ocs}"
 # defaults to just using the 'oc' binary provided in $PATH
 OCS_OC_PATH="${OCS_OC_PATH:-oc}"
 OCS_FINAL_DIR="deploy/olm-catalog/ocs-operator/manifests"
-BUNDLEMANIFESTS_DIR="deploy/bundlemanifests"
+BUNDLEMANIFESTS_DIR="rbac"
 
 NOOBAA_CSV="$OUTDIR_TEMPLATES/noobaa-csv.yaml"
 ROOK_CSV="$OUTDIR_TEMPLATES/rook-csv.yaml.in"
