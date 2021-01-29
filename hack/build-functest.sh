8a8a4bf453100adbd6028fd14b25557247a7f8fa
Umanga Chapagain <chapagainumanga@gmail.com>
2021-01-29 13:18:50 +0530

    Use Golang 1.15	and fix test dependencies
    Golang 1.15 has security fixes to Golang std library.
    
    Currently, `go install` is unable to install ginkgo which
    causes build failures during E2E tests. The workaround is
    to install ginkgo and gomega using `go get` and turn off
    the modules so that it doesn't update go.mod and go.sum files.
    
    Once we start using Go 1.16 we can switch back to `go install`
    as it is improved and recommended way to install dependencies.
    
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- hack/build-functest.sh
+++ hack/build-functest.sh
@@ -10,8 +10,13 @@ GINKGO=$GOBIN/ginkgo
 
 if ! [ -x "$GINKGO" ]; then
 	echo "Retrieving ginkgo and gomega build dependencies"
-	go install github.com/onsi/ginkgo/ginkgo
-	go install github.com/onsi/gomega/...
+	# TODO: Move to `go install` while upgrading to Go 1.16
+	# Currently, `go install` is unable to install ginkgo which
+	# causes build failures during E2E tests. The workaround is
+	# to install ginkgo and gomega using `go get` and turn off
+	# the modules so that it doesn't update go.mod and go.sum files
+	GO111MODULE=off go get github.com/onsi/ginkgo/ginkgo
+	GO111MODULE=off go get github.com/onsi/gomega/...
 else
 	echo "GINKO binary found at $GINKGO"
 fi
