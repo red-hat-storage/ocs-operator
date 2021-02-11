56f910a0ce3d842625791339cf83b2cd1e3c7bcc
Nitin Goyal <nigoyal@redhat.com>
2021-02-11 20:28:26 +0530

    storagecluster: Change event type from error to warning
    Fix Error via changing event type
    "E0210 23:17:48.027136       1 event.go:334] Unsupported event type: 'Error'"
    
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- controllers/util/events.go
+++ controllers/util/events.go
@@ -1,9 +1,8 @@
 package util
 
 const (
-
-	// EventTypeError is used to alert user of failures that require user action
-	EventTypeError = "Error"
+	// EventTypeWarning is used to alert user of failures that require user action
+	EventTypeWarning = "Warning"
 
 	// EventReasonValidationFailed is used when the StorageCluster spec validation fails
 	EventReasonValidationFailed = "FailedValidation"
