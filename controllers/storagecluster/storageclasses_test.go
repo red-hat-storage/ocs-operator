63038a5952c8d42aba2c98fa0ad677645a495049
Jose A. Rivera <jarrpa@redhat.com>
2021-03-29 12:26:05 -0500

    refactor SC unit test to loop over SCs
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- controllers/storagecluster/storageclasses_test.go
+++ controllers/storagecluster/storageclasses_test.go
@@ -25,61 +25,59 @@ func TestStorageClasses(t *testing.T) {
 }
 
 func assertStorageClasses(t *testing.T, reconciler StorageClusterReconciler, cr *api.StorageCluster, request reconcile.Request) {
-	actualSc1 := &storagev1.StorageClass{}
-	actualSc2 := &storagev1.StorageClass{}
-	actualSc3 := &storagev1.StorageClass{}
+	scNameCephfs := generateNameForCephFilesystemSC(cr)
+	scNameRbd := generateNameForCephBlockPoolSC(cr)
+	scNameRgw := generateNameForCephRgwSC(cr)
 
-	request.Name = "ocsinit-cephfs"
-	err := reconciler.Client.Get(context.TODO(), request.NamespacedName, actualSc1)
-	assert.NoError(t, err)
-
-	request.Name = "ocsinit-ceph-rbd"
-	err = reconciler.Client.Get(context.TODO(), request.NamespacedName, actualSc2)
-	assert.NoError(t, err)
-
+	actual := map[string]*storagev1.StorageClass{
+		scNameCephfs:   {},
+		scNameRbd:      {},
+		scNameRgw:      {},
+	}
 	expected, err := reconciler.newStorageClassConfigurations(cr)
 	assert.NoError(t, err)
-	request.Name = "ocsinit-ceph-rgw"
-	err = reconciler.Client.Get(context.TODO(), request.NamespacedName, actualSc3)
+
 	// on a cloud platform, 'Get' should throw an error,
-	// as OBC StorageClass won't be created
+	// as RGW StorageClass won't be created
 	avoid, avoidErr := reconciler.PlatformsShouldAvoidObjectStore()
 	assert.NoError(t, avoidErr)
 	if avoid {
-		// we should be expecting only 2 storage classes
-		assert.Equal(t, len(expected), 2)
-		assert.Error(t, err)
-	} else {
-		// if not a cloud platform, OBC Storage class should be created/updated
+		// we should be expecting only 3 StorageClasses
 		assert.Equal(t, len(expected), 3)
-		assert.NoError(t, err)
-		assert.Equal(t, len(expected[2].storageClass.OwnerReferences), 0)
-		assert.Equal(t, expected[2].storageClass.Name, actualSc3.ObjectMeta.Name)
-		assert.Equal(t, expected[2].storageClass.Provisioner, actualSc3.Provisioner)
-		assert.Equal(t, expected[2].storageClass.ReclaimPolicy, actualSc3.ReclaimPolicy)
-		assert.Equal(t, expected[2].storageClass.Parameters, actualSc3.Parameters)
-		// Doing a bit more validation for the RGW SC since some fields differ whether
-		// we do independent or converged mode, typically "objectStoreName" param must exist
-		assert.NotEmpty(t, actualSc3.Parameters["objectStoreName"], actualSc3.Parameters)
-		assert.NotEmpty(t, actualSc3.Parameters["region"], actualSc3.Parameters)
-		assert.Equal(t, 3, len(actualSc3.Parameters))
+	} else {
+		// if not a cloud platform, RGW StorageClass should be created/updated
+		assert.Equal(t, len(expected), 4)
 	}
 
-	// The created StorageClasses should not have any ownerReferences set. Any
-	// OwnerReference set will be a cross-namespace OwnerReference, which could
-	// lead to other child resources getting GCd.
-	// Ref: https://bugzilla.redhat.com/show_bug.cgi?id=1755623
-	// Ref: https://bugzilla.redhat.com/show_bug.cgi?id=1691546
-	assert.Equal(t, len(expected[0].storageClass.OwnerReferences), 0)
-	assert.Equal(t, len(expected[1].storageClass.OwnerReferences), 0)
+	for _, scConfig := range expected {
+		scName := scConfig.storageClass.Name
+		request.Name = scName
+		err := reconciler.Client.Get(context.TODO(), request.NamespacedName, actual[scName])
+		if avoid && scName == scNameRgw {
+			assert.Error(t, err)
+		} else {
+			assert.NoError(t, err)
+			actualSc := actual[scName]
+			expectedSc := scConfig.storageClass
 
-	assert.Equal(t, expected[0].storageClass.ObjectMeta.Name, actualSc1.ObjectMeta.Name)
-	assert.Equal(t, expected[0].storageClass.Provisioner, actualSc1.Provisioner)
-	assert.Equal(t, expected[0].storageClass.ReclaimPolicy, actualSc1.ReclaimPolicy)
-	assert.Equal(t, expected[0].storageClass.Parameters, actualSc1.Parameters)
+			// The created StorageClasses should not have any ownerReferences set. Any
+			// OwnerReference set will be a cross-namespace OwnerReference, which could
+			// lead to other child resources getting GCd.
+			// Ref: https://bugzilla.redhat.com/show_bug.cgi?id=1755623
+			// Ref: https://bugzilla.redhat.com/show_bug.cgi?id=1691546
+			assert.Equal(t, len(actualSc.OwnerReferences), 0)
 
-	assert.Equal(t, expected[1].storageClass.ObjectMeta.Name, actualSc2.ObjectMeta.Name)
-	assert.Equal(t, expected[1].storageClass.Provisioner, actualSc2.Provisioner)
-	assert.Equal(t, expected[1].storageClass.ReclaimPolicy, actualSc2.ReclaimPolicy)
-	assert.Equal(t, expected[1].storageClass.Parameters, actualSc2.Parameters)
+			assert.Equal(t, scName, actualSc.ObjectMeta.Name)
+			assert.Equal(t, expectedSc.Provisioner, actualSc.Provisioner)
+			assert.Equal(t, expectedSc.ReclaimPolicy, actualSc.ReclaimPolicy)
+			assert.Equal(t, expectedSc.Parameters, actualSc.Parameters)
+			if scName == scNameRgw {
+				// Doing a bit more validation for the RGW SC since some fields differ whether
+				// we do independent or converged mode, typically "objectStoreName" param must exist
+				assert.NotEmpty(t, actualSc.Parameters["objectStoreName"], actualSc.Parameters)
+				assert.NotEmpty(t, actualSc.Parameters["region"], actualSc.Parameters)
+				assert.Equal(t, 3, len(actualSc.Parameters))
+			}
+		}
+	}
 }
