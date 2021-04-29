537419ca7dee7f85ce45a5336aa752f9ab6087bb
Nitin Goyal <nigoyal@redhat.com>
2021-04-29 22:34:47 +0530

    hack: Change the order of the IMAGE_BUILD_CMD
    check if podman exists first and then check for docker if podman not
    found.
    
    Signed-off-by: Nitin Goyal <nigoyal@redhat.com>

--- hack/docker-common.sh
+++ hack/docker-common.sh
@@ -4,10 +4,10 @@
 # disable unused variable warnings
 
 if [ -z "$IMAGE_BUILD_CMD" ]; then
-    IMAGE_BUILD_CMD=$(command -v docker || echo "")
+    IMAGE_BUILD_CMD=$(command -v podman || echo "")
 fi
 if [ -z "$IMAGE_BUILD_CMD" ]; then
-    IMAGE_BUILD_CMD=$(command -v podman || echo "")
+    IMAGE_BUILD_CMD=$(command -v docker || echo "")
 fi
 
 IMAGE_RUN_CMD="${IMAGE_RUN_CMD:-${IMAGE_BUILD_CMD} run --rm -it}"
