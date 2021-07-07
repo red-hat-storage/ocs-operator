7d3b63fab99a93cb8f348fb9fe7d038b0eb5ccb7
Kaustav Majumder <kmajumder@redhat.com>
2021-07-07 13:44:49 +0530

    Added DBStorageClassName to MultiCloudGatewaySpec to specify the default storageclass to be used for noobaa-db pods
    Signed-off-by: Kaustav Majumder <kmajumder@redhat.com>

--- controllers/storagecluster/noobaa_system_reconciler_test.go
+++ controllers/storagecluster/noobaa_system_reconciler_test.go
@@ -23,9 +23,10 @@ import (
 )
 
 const (
-	coreEnvVar          = "NOOBAA_CORE_IMAGE"
-	dbEnvVar            = "NOOBAA_DB_IMAGE"
-	defaultStorageClass = "noobaa-ceph-rbd"
+	coreEnvVar                 = "NOOBAA_CORE_IMAGE"
+	dbEnvVar                   = "NOOBAA_DB_IMAGE"
+	defaultStorageClass        = "noobaa-ceph-rbd"
+	noobaDbDefaultStorageClass = "test-storage-class"
 )
 
 var noobaaReconcileTestLogger = logf.Log.WithName("noobaa_system_reconciler_test")
@@ -238,10 +239,11 @@ func TestSetNooBaaDesiredState(t *testing.T) {
 		},
 	}
 	cases := []struct {
-		label   string
-		envCore string
-		envDB   string
-		sc      v1.StorageCluster
+		label              string
+		envCore            string
+		envDB              string
+		sc                 v1.StorageCluster
+		dbStorageClassName string
 	}{
 		{
 			label:   "case 1", // both envVars carry through to created NooBaaSystem
@@ -266,6 +268,17 @@ func TestSetNooBaaDesiredState(t *testing.T) {
 		{
 			label: "case 5", // missing initData namespace does not cause error
 			sc:    v1.StorageCluster{},
+		},
+		{
+			label: "case 6", // dbStorageClassName should reflect DBStorageClass in nooba spec
+			sc: v1.StorageCluster{
+				Spec: v1.StorageClusterSpec{
+					MultiCloudGateway: &v1.MultiCloudGatewaySpec{
+						DbStorageClassName: noobaDbDefaultStorageClass,
+					},
+				},
+			},
+			dbStorageClassName: noobaDbDefaultStorageClass,
 		},
 	}
 
@@ -299,16 +312,18 @@ func TestSetNooBaaDesiredState(t *testing.T) {
 		if err != nil {
 			assert.Failf(t, "[%s] unable to set noobaa desired state", c.label)
 		}
-
+		if c.dbStorageClassName == "" {
+			c.dbStorageClassName = generateNameForCephBlockPoolSC(&c.sc, "")
+		}
 		assert.Equalf(t, noobaa.Name, "noobaa", "[%s] noobaa name not set correctly", c.label)
 		assert.NotEmptyf(t, noobaa.Labels, "[%s] expected noobaa Labels not found", c.label)
 		assert.Equalf(t, noobaa.Labels["app"], "noobaa", "[%s] expected noobaa Label mismatch", c.label)
 		assert.Equalf(t, noobaa.Name, "noobaa", "[%s] noobaa name not set correctly", c.label)
-		assert.Equal(t, *noobaa.Spec.DBStorageClass, fmt.Sprintf("%s-ceph-rbd", c.sc.Name))
+		assert.Equal(t, *noobaa.Spec.DBStorageClass, c.dbStorageClassName)
+		assert.Equal(t, *noobaa.Spec.PVPoolDefaultStorageClass, c.dbStorageClassName)
 		noobaaplacement := getPlacement(&c.sc, "noobaa-core")
 		assert.Equal(t, noobaa.Spec.Tolerations, noobaaplacement.Tolerations)
 		assert.Equal(t, noobaa.Spec.Affinity, &corev1.Affinity{NodeAffinity: noobaaplacement.NodeAffinity})
-		assert.Equal(t, *noobaa.Spec.PVPoolDefaultStorageClass, fmt.Sprintf("%s-ceph-rbd", c.sc.Name))
 		assert.Equalf(t, noobaa.Namespace, c.sc.Namespace, "[%s] namespace mismatch", c.label)
 		if c.envCore != "" {
 			assert.Equalf(t, *noobaa.Spec.Image, c.envCore, "[%s] core envVar not applied to noobaa spec", c.label)
