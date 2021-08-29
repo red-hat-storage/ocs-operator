cc6dc91381b2890cbbc806218106d1c8a163cbaf
Jose A. Rivera <jarrpa@redhat.com>
2021-08-29 13:06:29 -0500

    hack: add noobaa-operator-bundle to operator index

--- hack/build-operator-index.sh
+++ hack/build-operator-index.sh
@@ -6,11 +6,13 @@ source hack/common.sh
 source hack/docker-common.sh
 source hack/ensure-opm.sh
 
+BUNDLE_IMGS="${BUNDLE_IMGS:-${BUNDLE_FULL_IMAGE_NAME},${NOOBAA_BUNDLE_FULL_IMAGE_NAME}}"
+
 echo
 echo "Did you push the bundle image? It must be pullable from '$IMAGE_REGISTRY'."
 echo "Run '${IMAGE_BUILD_CMD} push ${BUNDLE_FULL_IMAGE_NAME}'"
 echo
-${OPM} index add --bundles "${BUNDLE_FULL_IMAGE_NAME}" --tag "${INDEX_FULL_IMAGE_NAME}"
+${OPM} index add --bundles "${BUNDLE_IMGS}" --tag "${INDEX_FULL_IMAGE_NAME}"
 
 echo
 echo "Run '${IMAGE_BUILD_CMD} push ${INDEX_FULL_IMAGE_NAME}' to push operator index to image registry."
