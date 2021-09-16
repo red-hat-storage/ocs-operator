4d4fcd119a1b7f8e26cb58466776e3024419b8ee
Mudit Agarwal <muagarwa@redhat.com>
2021-09-16 19:00:35 +0530

    fixing arguments while calling generateNameForCephBlockPoolSC()
    Signed-off-by: Mudit Agarwal <muagarwa@redhat.com>

--- controllers/storagecluster/noobaa_system_reconciler_test.go
+++ controllers/storagecluster/noobaa_system_reconciler_test.go
@@ -313,7 +313,7 @@ func TestSetNooBaaDesiredState(t *testing.T) {
 			assert.Failf(t, "[%s] unable to set noobaa desired state", c.label)
 		}
 		if c.dbStorageClassName == "" {
-			c.dbStorageClassName = generateNameForCephBlockPoolSC(&c.sc, "")
+			c.dbStorageClassName = generateNameForCephBlockPoolSC(&c.sc)
 		}
 		assert.Equalf(t, noobaa.Name, "noobaa", "[%s] noobaa name not set correctly", c.label)
 		assert.NotEmptyf(t, noobaa.Labels, "[%s] expected noobaa Labels not found", c.label)
