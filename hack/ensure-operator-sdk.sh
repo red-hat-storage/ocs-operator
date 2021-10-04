6aaf11e44939106d7510fa5c705daa31a54e40ae
Shachar Sharon <ssharon@redhat.com>
2021-10-04 16:46:46 +0300

    build: support AWS/Graviton (aarch64)
    Build OCS on AWS/Graviton processor (ARM, aarch64). Need to update usage
    of external tools:
    
    1) Download proper operator-sdk for local arch, make sure that the
       downloaded image is indeed exacutable on local host.
    2) For aarch64 need to build opm on local machine as no image is
       provided.
    
    Align definition of OS & ARCH with installation instruction at:
      https://sdk.operatorframework.io/docs/installation/
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- hack/ensure-operator-sdk.sh
+++ hack/ensure-operator-sdk.sh
@@ -6,10 +6,13 @@ source hack/common.sh
 source hack/operator-sdk-common.sh
 
 if [ ! -x "${OPERATOR_SDK}" ]; then
-	echo "Downloading operator-sdk ${OPERATOR_SDK_VERSION}-${OPERATOR_SDK_PLATFORM}"
-	mkdir -p ${OUTDIR_TOOLS}
-	curl -JL "${OPERATOR_SDK_URL}/${OPERATOR_SDK_VERSION}/${OPERATOR_SDK_BIN}" -o "${OPERATOR_SDK}"
+	echo "Downloading operator-sdk: ${OPERATOR_SDK_DL_URL_FULL} --> ${OPERATOR_SDK}"
+	mkdir -p "${OUTDIR_TOOLS}"
+	curl -JL "${OPERATOR_SDK_DL_URL_FULL}" -o "${OPERATOR_SDK}"
 	chmod +x "${OPERATOR_SDK}"
 else
 	echo "Using operator-sdk cached at ${OPERATOR_SDK}"
 fi
+
+# Ensure operator-sdk can run properly on local machine
+"${OPERATOR_SDK}" version > /dev/null 2>&1 || echo "Bad operator-sdk binary: ${OPERATOR_SDK}"
