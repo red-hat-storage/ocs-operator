fcb4b9e7941c99ac4348b6fc69f99551eeb8c764
Shachar Sharon <ssharon@redhat.com>
2021-10-04 16:46:46 +0300

    build: support AWS/Graviton (aarch64)
    Build OCS on AWS/Graviton processor (ARM, aarch64). Need to update usage
    of external tools:
    
    1) Download proper operator-sdk for local arch, make sure that the
       downloaded image is indeed exacutable on local host.
    2) For aarch64 need to build opm on local machine as no image is
       provided.
    
    Align definition of OS & ARCH with installation instruction at:
      https://sdk.operatorframework.io/docs/installation/
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- hack/operator-sdk-common.sh
+++ hack/operator-sdk-common.sh
@@ -3,11 +3,19 @@
 # shellcheck disable=SC2034
 # don't fail on unused variables - this is for sourcing
 
+
+# Align with installation instructions from:
+# https://sdk.operatorframework.io/docs/installation/
+OPERATOR_SDK_ARCH=$(uname -m)
+OPERATOR_SDK_OS=$(uname | awk '{print tolower($0)}')
+# This hack was changed in newer operator-sdk versions
+if [ "$OPERATOR_SDK_OS" == "linux" ]; then
+       OPERATOR_SDK_OS="linux-gnu"
+elif [ "$OPERATOR_SDK_OS" == "darwin" ]; then
+       OPERATOR_SDK_OS="apple-darwin"
+fi
+
 OPERATOR_SDK_URL="${OPERATOR_SDK_URL:-https://github.com/operator-framework/operator-sdk/releases/download}"
 OPERATOR_SDK_VERSION="${OPERATOR_SDK_VERSION:-v1.2.0}"
-OPERATOR_SDK_PLATFORM="x86_64-linux-gnu"
-if [ "$OS_TYPE" == "Darwin" ]; then
-	OPERATOR_SDK_PLATFORM="x86_64-apple-darwin"
-fi
-OPERATOR_SDK_BIN="operator-sdk-${OPERATOR_SDK_VERSION}-${OPERATOR_SDK_PLATFORM}"
+OPERATOR_SDK_DL_URL_FULL="${OPERATOR_SDK_URL}/${OPERATOR_SDK_VERSION}/operator-sdk-${OPERATOR_SDK_VERSION}-${OPERATOR_SDK_ARCH}-${OPERATOR_SDK_OS}"
 OPERATOR_SDK="${OUTDIR_TOOLS}/operator-sdk-${OPERATOR_SDK_VERSION}"
