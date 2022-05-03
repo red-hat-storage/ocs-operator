31e0544c0e95571580b167dda54a576129feff8b
Jose A. Rivera <jarrpa@redhat.com>
2022-05-03 11:39:47 -0500

    csv: update noobaa images since the previous tags have disappeared
    Since this only affects the CSV generation in the upstream, this should
    not have an impact on the downstream product builds.
    
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- hack/common.sh
+++ hack/common.sh
@@ -53,8 +53,8 @@ ROOK_CSV="$OUTDIR_TEMPLATES/rook-csv.yaml.in"
 OCS_CSV="$OUTDIR_TEMPLATES/ocs-operator.csv.yaml.in"
 
 LATEST_ROOK_IMAGE="rook/ceph:v1.6.0.95.gf4cfc7a"
-LATEST_NOOBAA_IMAGE="noobaa/noobaa-operator:master-20210419"
-LATEST_NOOBAA_CORE_IMAGE="noobaa/noobaa-core:master-20210419"
+LATEST_NOOBAA_IMAGE="noobaa/noobaa-operator:5.8_special_backport-20220325"
+LATEST_NOOBAA_CORE_IMAGE="noobaa/noobaa-core:5.8_special_backport-20220325"
 LATEST_NOOBAA_DB_IMAGE="centos/postgresql-12-centos7"
 # The stretch cluster feature will come in ceph pacific(v16).  We don't have an
 # image for it yet. Meanwhile, we will use an image that has the required
