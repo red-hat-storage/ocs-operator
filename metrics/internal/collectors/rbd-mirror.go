53d1d7982c51727d023f3ac970098e4efe8b1e00
Umanga Chapagain <chapagainumanga@gmail.com>
2022-05-19 09:35:08 +0530

    metrics: always export rbd mirror state metrics
    RBD mirror state might not always be primary. So we can not
    use that as a check to export metrics. There are cases like
    split brain or demoted image in which case we still need to
    export state metrics. So, this commit removes the check.
    
    However, other information are only available when the image
    is primary and so the checks are required.
    
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- metrics/internal/collectors/rbd-mirror.go
+++ metrics/internal/collectors/rbd-mirror.go
@@ -119,53 +119,53 @@ func (c *RBDMirrorCollector) Collect(ch chan<- prometheus.Metric) {
 			}
 		}
 		for _, image := range poolData.MirrorStatus.Images {
-			if image.Description == "local image is primary" {
-				for _, site := range image.PeerSites {
-					// site.State can have values like up+stopped, up+error etc.
-					state := strings.SplitN(site.State, "+", 2)
-					if len(state) != 2 {
-						klog.Errorf("Unexpected mirror state %q of image %q to site %q.", site.State, image.Name, site.SiteName)
-					} else {
-						switch state[1] {
-						case "unknown":
-							ch <- prometheus.MustNewConstMetric(
-								c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateUnknown,
-								image.Name, poolData.PoolName, site.SiteName,
-							)
-						case "error":
-							ch <- prometheus.MustNewConstMetric(
-								c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateError,
-								image.Name, poolData.PoolName, site.SiteName,
-							)
-						case "syncing":
-							ch <- prometheus.MustNewConstMetric(
-								c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateSyncing,
-								image.Name, poolData.PoolName, site.SiteName,
-							)
-						case "starting_replay":
-							ch <- prometheus.MustNewConstMetric(
-								c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateStartingReplay,
-								image.Name, poolData.PoolName, site.SiteName,
-							)
-						case "replaying":
-							ch <- prometheus.MustNewConstMetric(
-								c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateReplaying,
-								image.Name, poolData.PoolName, site.SiteName,
-							)
-						case "stopping_replay":
-							ch <- prometheus.MustNewConstMetric(
-								c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateStoppingReplay,
-								image.Name, poolData.PoolName, site.SiteName,
-							)
-						case "stopped":
-							ch <- prometheus.MustNewConstMetric(
-								c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateStopped,
-								image.Name, poolData.PoolName, site.SiteName,
-							)
-						default:
-							klog.Errorf("Unknown state %q of image %q", site.State, image.Name)
-						}
+			for _, site := range image.PeerSites {
+				// site.State can have values like up+stopped, up+error etc.
+				state := strings.SplitN(site.State, "+", 2)
+				if len(state) != 2 {
+					klog.Errorf("Unexpected mirror state %q of image %q to site %q.", site.State, image.Name, site.SiteName)
+				} else {
+					switch state[1] {
+					case "unknown":
+						ch <- prometheus.MustNewConstMetric(
+							c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateUnknown,
+							image.Name, poolData.PoolName, site.SiteName,
+						)
+					case "error":
+						ch <- prometheus.MustNewConstMetric(
+							c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateError,
+							image.Name, poolData.PoolName, site.SiteName,
+						)
+					case "syncing":
+						ch <- prometheus.MustNewConstMetric(
+							c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateSyncing,
+							image.Name, poolData.PoolName, site.SiteName,
+						)
+					case "starting_replay":
+						ch <- prometheus.MustNewConstMetric(
+							c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateStartingReplay,
+							image.Name, poolData.PoolName, site.SiteName,
+						)
+					case "replaying":
+						ch <- prometheus.MustNewConstMetric(
+							c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateReplaying,
+							image.Name, poolData.PoolName, site.SiteName,
+						)
+					case "stopping_replay":
+						ch <- prometheus.MustNewConstMetric(
+							c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateStoppingReplay,
+							image.Name, poolData.PoolName, site.SiteName,
+						)
+					case "stopped":
+						ch <- prometheus.MustNewConstMetric(
+							c.ImageStatusState, prometheus.GaugeValue, mirrorImageStatusStateStopped,
+							image.Name, poolData.PoolName, site.SiteName,
+						)
+					default:
+						klog.Errorf("Unknown state %q of image %q", site.State, image.Name)
 					}
+				}
+				if image.Description == "local image is primary" {
 					// site.Description can have values like "replaying,{...}" "split-brain" etc.
 					siteDescription := strings.SplitN(site.Description, ", ", 2)
 					if len(siteDescription) != 2 {
