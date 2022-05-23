b4d88de09731e3ae3d8ad531d6dacf692cfa7904
Malay Kumar Parida <mparida@redhat.com>
2022-05-23 16:13:39 +0530

    Add the defaults for rook-ceph operator in the csv
    Values like CSI_LOG_LEVEL & CSI_ENABLE_CSIADDONS will be
    configured with defaults in the csv generation it self as ENV
    variables.
    
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- controllers/ocsinitialization/ocsinitialization_controller.go
+++ controllers/ocsinitialization/ocsinitialization_controller.go
@@ -9,7 +9,6 @@ import (
 	"github.com/go-logr/logr"
 	secv1client "github.com/openshift/client-go/security/clientset/versioned/typed/security/v1"
 	ocsv1 "github.com/red-hat-storage/ocs-operator/api/v1"
-	"github.com/red-hat-storage/ocs-operator/controllers/defaults"
 	"github.com/red-hat-storage/ocs-operator/controllers/util"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
