7cf3b1062ee3101b4ea6b139ceaac6ae6f8bbe96
Jose A. Rivera <jarrpa@redhat.com>
2022-06-17 10:57:39 -0500

    e2e: update to use 'go install' and be more verbose
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- hack/build-functest.sh
+++ hack/build-functest.sh
@@ -10,19 +10,14 @@ GINKGO=$GOBIN/ginkgo
 
 if ! [ -x "$GINKGO" ]; then
 	echo "Retrieving ginkgo and gomega build dependencies"
-	# TODO: Move to `go install` while upgrading to Go 1.16
-	# Currently, `go install` is unable to install ginkgo which
-	# causes build failures during E2E tests. The workaround is
-	# to install ginkgo and gomega using `go get` and turn off
-	# the modules so that it doesn't update go.mod and go.sum files
-	GO111MODULE=off go get github.com/onsi/ginkgo/ginkgo
-	GO111MODULE=off go get github.com/onsi/gomega/...
+	go install -v github.com/onsi/ginkgo/v2/ginkgo@latest
+	go install -v github.com/onsi/gomega/...
 else
 	echo "GINKO binary found at $GINKGO"
 fi
 
 
-"$GOBIN"/ginkgo build "functests/${suite}/"
+"${GINKGO}" build "functests/${suite}/"
 
 mkdir -p $OUTDIR_BIN
 mv "functests/${suite}/${suite}.test" "${OUTDIR_BIN}/${suite}_tests"
