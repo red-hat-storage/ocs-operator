01783d9e5e993f4cc64097f191ab1cfcaf1b9bbe
Dhruv Bindra <dbindra@redhat.com>
2022-08-10 13:14:53 +0530

    Initialize rookCephOperatorCM if data is nil
    Signed-off-by: Dhruv Bindra <dbindra@redhat.com>

--- controllers/storagecluster/external_resources.go
+++ controllers/storagecluster/external_resources.go
@@ -66,6 +66,9 @@ func (r *StorageClusterReconciler) setRookCSICephFS(
 	if rookCephOperatorConfig.Data[rookEnableCephFSCSIKey] == enableDisableFlagStr {
 		return nil
 	}
+	if rookCephOperatorConfig.Data == nil {
+		rookCephOperatorConfig.Data = map[string]string{}
+	}
 	rookCephOperatorConfig.Data[rookEnableCephFSCSIKey] = enableDisableFlagStr
 	return r.Client.Update(context.TODO(), rookCephOperatorConfig)
 }
