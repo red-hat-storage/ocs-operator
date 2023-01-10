4a5dd2e1e2b60e0a50a50ecd8af1c5f447e4b349
Umanga Chapagain <chapagainumanga@gmail.com>
2023-01-10 12:25:36 +0530

    change ocs-operator bundle path
    Moving ocs-operator bundle to deploy/ocs-operator. This is to allow
    other bundles to be generated under deploy/ without mixing it with
    ocs-operator bundle manifests.
    
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- hack/source-manifests.sh
+++ hack/source-manifests.sh
@@ -26,7 +26,6 @@ fi
 
 # always start fresh and remove any previous artifacts that may exist.
 rm -rf "$(dirname $OCS_FINAL_DIR)"
-mkdir -p "$(dirname $OCS_FINAL_DIR)"
 mkdir -p $OUTDIR_TEMPLATES
 mkdir -p $OUTDIR_CRDS
 mkdir -p $OUTDIR_TOOLS
@@ -77,20 +76,21 @@ function dump_rook_csv() {
 # Generate an OCS CSV using the operator-sdk.
 # This is the base CSV everything else gets merged into later on.
 function gen_ocs_csv() {
+	echo "Generating OpenShift Container Storage CSV"
 	ocs_crds_outdir="$OUTDIR_CRDS/ocs"
 	rm -rf $OUTDIR_TEMPLATES/manifests/ocs-operator.clusterserviceversion.yaml
 	rm -rf $OCS_CSV
 	rm -rf $ocs_crds_outdir
 	mkdir -p $ocs_crds_outdir
 
-	gen_args="generate kustomize manifests -q"
+	gen_args="generate kustomize manifests --input-dir config/manifests/ocs-operator --output-dir config/manifests/ocs-operator --package ocs-operator -q"
 	# shellcheck disable=SC2086
 	$OPERATOR_SDK $gen_args
 	pushd config/manager
 	$KUSTOMIZE edit set image ocs-dev/ocs-operator="$OCS_IMAGE"
 	popd
-	$KUSTOMIZE build config/manifests | $OPERATOR_SDK generate bundle -q --overwrite=false --version "$CSV_VERSION" --extra-service-accounts=ocs-metrics-exporter
-	mv bundle/manifests/*clusterserviceversion.yaml $OCS_CSV
+	$KUSTOMIZE build config/manifests/ocs-operator | $OPERATOR_SDK generate bundle -q --overwrite=false --output-dir deploy/ocs-operator --kustomize-dir config/manifests/ocs-operator --package ocs-operator --version "$CSV_VERSION" --extra-service-accounts=ocs-metrics-exporter
+	mv deploy/ocs-operator/manifests/*clusterserviceversion.yaml $OCS_CSV
 	cp config/crd/bases/* $ocs_crds_outdir
 }
 
@@ -104,8 +104,4 @@ gen_ocs_csv
 
 echo "Manifests sourced into $OUTDIR_TEMPLATES directory"
 
-
-mv bundle/manifests $OCS_FINAL_DIR
-mv bundle/metadata "$(dirname $OCS_FINAL_DIR)"/metadata
-rm -rf bundle
 rm bundle.Dockerfile
