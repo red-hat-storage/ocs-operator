red-hat-storage/ocs-operator

commit 50dcce2d5c4d8f0c9d2853ab420a8528ac3d950d

build and verify fusion bundle image
This commit updates make targets to receive and forward a user entered
value for FUSION to underlying bundle build and verify scripts.

If we run `FUSION=true make operator-bundle` it generates fusion
container storage bundle image. This bundle can be verified using
`FUSION=true make verify-operator-bundle`.

Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

hack/build-operator-bundle.sh
@@ -8,11 +8,23 @@ source hack/docker-common.sh
 TMP_ROOT="$(dirname "${BASH_SOURCE[@]}")/.."
 REPO_ROOT=$(readlink -e "${TMP_ROOT}" 2> /dev/null || perl -MCwd -e 'print Cwd::abs_path shift' "${TMP_ROOT}")
 
-pushd "${REPO_ROOT}/deploy"
-$IMAGE_BUILD_CMD build --no-cache -t "$BUNDLE_FULL_IMAGE_NAME" -f Dockerfile .
+function build_operator_bundle() {
+    DOCKERFILE=$1
 
-echo
-echo "Run '${IMAGE_BUILD_CMD} push ${BUNDLE_FULL_IMAGE_NAME}' to push operator bundle to image registry."
-echo
-echo "Run './hack/build-operator-index.sh' to add this bundle to operator index."
-popd
+    pushd "${REPO_ROOT}/deploy"
+    $IMAGE_BUILD_CMD build --no-cache -t "$BUNDLE_FULL_IMAGE_NAME" -f "$DOCKERFILE" .
+
+    echo
+    echo "Run '${IMAGE_BUILD_CMD} push ${BUNDLE_FULL_IMAGE_NAME}' to push operator bundle to image registry."
+    echo
+    echo "Run './hack/build-operator-index.sh' to add this bundle to operator index."
+    popd
+}
+
+if [ "$FUSION" == true ]; then
+    echo "Building IBM Fusion Container Storage Operator Bundle"
+    build_operator_bundle fcs.Dockerfile
+else
+    echo "Building Red Hat OpenShift Container Storage Operator Bundle"
+    build_operator_bundle ocs.Dockerfile
+fi

Makefile
@@ -11,6 +11,7 @@ else
 GOBIN=$(shell go env GOBIN)
 endif
 
+FUSION?=false
 KUSTOMIZE_VERSION=v4.5.2
 CONTROLLER_GEN_VERSION=v0.9.2
 
@@ -118,11 +119,11 @@ verify-operator-bundle: operator-sdk
 	hack/verify-operator-bundle.sh
 
 operator-bundle:
-	@echo "Building ocs-operator-bundle image"
+	@echo "FUSION=$(FUSION)"
 	hack/build-operator-bundle.sh
 
 operator-index:
-	@echo "Building ocs-operator-index image"
+	@echo "FUSION=$(FUSION)"
 	hack/build-operator-index.sh
 
 ocs-registry:

deploy/fcs.Dockerfile
@@ -1,0 +1,19 @@
+FROM scratch
+
+LABEL operators.operatorframework.io.bundle.mediatype.v1=registry+v1
+LABEL operators.operatorframework.io.bundle.manifests.v1=manifests/
+LABEL operators.operatorframework.io.bundle.metadata.v1=metadata/
+LABEL operators.operatorframework.io.bundle.package.v1=fcs-operator
+LABEL operators.operatorframework.io.bundle.channels.v1=alpha
+LABEL operators.operatorframework.io.bundle.channel.default.v1=alpha
+
+COPY fcs-operator/manifests /manifests/
+COPY fcs-operator/metadata/annotations.yaml /metadata/annotations.yaml
+
+# These are three labels needed to control how the pipeline should handle this container image
+# This first label tells the pipeline that this is a bundle image and should be
+# delivered via an index image
+LABEL com.redhat.delivery.operator.bundle=true
+# This second label tells the pipeline which versions of OpenShift the operator supports.
+# This is used to control which index images should include this operator.
+LABEL com.redhat.openshift.versions="v4.12"

hack/verify-operator-bundle.sh
@@ -5,4 +5,8 @@ set -e
 source hack/ensure-operator-sdk.sh
 source hack/docker-common.sh
 
-./"${OPERATOR_SDK}" bundle validate "$(dirname $OCS_FINAL_DIR)" --verbose
+if [ "$FUSION" == "true" ]; then
+    ./"${OPERATOR_SDK}" bundle validate "$(dirname $FCS_FINAL_DIR)" --verbose
+else
+    ./"${OPERATOR_SDK}" bundle validate "$(dirname $OCS_FINAL_DIR)" --verbose
+fi

commit 1acb627782f96589c6412979595e438fe3b34857

fix OCS Dockerfile path in bundle build script
This commit fixes the Dockerfile path in the bundle build script which
was broken after commit 50dcce2d5.

Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

hack/build-operator-bundle.sh
@@ -26,5 +26,5 @@ if [ "$FUSION" == true ]; then
     build_operator_bundle fcs.Dockerfile
 else
     echo "Building Red Hat OpenShift Container Storage Operator Bundle"
-    build_operator_bundle ocs.Dockerfile
+    build_operator_bundle Dockerfile
 fi