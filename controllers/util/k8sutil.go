795a3b287a4e1c7425b36d35bb4aa81f30fc880d
Rakshith R <rar@redhat.com>
2023-02-17 10:29:28 +0530

    cephcluster: add support for read affinity for RBD volumes
    This commit adds support for read affinity for RBD volumes by
    settings the "CSI_ENABLE_READ_AFFINITY" variable to "true" for
    internal cluster and "false" for external cluster in the
    ocs-operator-config ConfigMap.
    
    Signed-off-by: Rakshith R <rar@redhat.com>

--- controllers/util/k8sutil.go
+++ controllers/util/k8sutil.go
@@ -1,14 +1,24 @@
 package util
 
 import (
+	"context"
 	"fmt"
 	"os"
+
+	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// WatchNamespaceEnvVar is the constant for env variable WATCH_NAMESPACE
-// which is the namespace where the watch activity happens.
-// this value is empty if the operator is running with clusterScope.
-const WatchNamespaceEnvVar = "WATCH_NAMESPACE"
+const (
+	// WatchNamespaceEnvVar is the constant for env variable WATCH_NAMESPACE
+	// which is the namespace where the watch activity happens.
+	// this value is empty if the operator is running with clusterScope.
+	WatchNamespaceEnvVar = "WATCH_NAMESPACE"
+
+	// This configmap is purely for the OCS operator to use.
+	OcsOperatorConfigName = "ocs-operator-config"
+)
 
 // GetWatchNamespace returns the namespace the operator should be watching for changes
 func GetWatchNamespace() (string, error) {
@@ -30,4 +40,21 @@ func GetOperatorNamespace() (string, error) {
 		return "", fmt.Errorf("%s must be set", OperatorNamespaceEnvVar)
 	}
 	return ns, nil
+}
+
+// RestartRookOperatorPod restarts the rook-operator pod in the OCP cluster
+func RestartRookOperatorPod(ctx context.Context, kubeClient client.Client, logger *logr.Logger, namespace string) {
+	podList := &corev1.PodList{}
+	err := kubeClient.List(ctx, podList, client.InNamespace(namespace), client.MatchingLabels{"app": "rook-ceph-operator"})
+	if err != nil {
+		logger.Error(err, "Failed to list rook-ceph-operator pod")
+		return
+	}
+	for _, pod := range podList.Items {
+		err := kubeClient.Delete(ctx, &pod)
+		if err != nil {
+			logger.Error(err, "Failed to delete rook-ceph-operator pod")
+			return
+		}
+	}
 }
