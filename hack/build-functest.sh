47d7f7377df6e8fa60133115607d1365c473fe12
Malay Kumar Parida <mparida@redhat.com>
2023-04-02 18:36:44 +0000

    ginkgo ci fix
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- hack/build-functest.sh
+++ hack/build-functest.sh
@@ -10,13 +10,7 @@ GINKGO=$GOBIN/ginkgo
 
 if ! [ -x "$GINKGO" ]; then
 	echo "Retrieving ginkgo and gomega build dependencies"
-	# TODO: Move to `go install` while upgrading to Go 1.16
-	# Currently, `go install` is unable to install ginkgo which
-	# causes build failures during E2E tests. The workaround is
-	# to install ginkgo and gomega using `go get` and turn off
-	# the modules so that it doesn't update go.mod and go.sum files
-	GO111MODULE=off go get github.com/onsi/ginkgo/ginkgo
-	GO111MODULE=off go get github.com/onsi/gomega/...
+	go install github.com/onsi/ginkgo/v2/ginkgo@latest
 else
 	echo "GINKO binary found at $GINKGO"
 fi
