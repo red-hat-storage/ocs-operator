2b3ad9e899b8f6f19e368e51a86de70f9a5e5c5e
Malay Kumar Parida <mparida@redhat.com>
2023-04-13 11:59:41 +0000

    Pass appropriate options for all the cases of Network Spec combination
    We have mainly 4 cases.
    1. Encryption is enabled
    2. Encryption is not enabled, but Either Compression is enabled or
        RequireMsgr2 is true
    3. None of Encryption, Compression, RequireMsgr2 is enabled, And it is
        an external or provider cluster
    4. None of the above cases are true, By default we will require Msgr2
    
    For all options appropriate options are passed for cephfs mounting.
    
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- controllers/storagecluster/ocs_operator_config.go
+++ controllers/storagecluster/ocs_operator_config.go
@@ -102,14 +102,29 @@ func (r *StorageClusterReconciler) getClusterID() string {
 	return fmt.Sprint(clusterVersion.Spec.ClusterID)
 }
 
-// getCephFSKernelMountOptions returns the kernel mount options for cephfs
+// getCephFSKernelMountOptions returns the kernel mount options for CephFS based on the spec on the StorageCluster
 func getCephFSKernelMountOptions(sc *ocsv1.StorageCluster) string {
-	// If it is an provider or consumer/external cluster, we don't need to set any mount options
+	// If Encryption is enabled, Always use secure mode
+	if sc.Spec.Network != nil && sc.Spec.Network.Connections != nil &&
+		sc.Spec.Network.Connections.Encryption.Enabled {
+		return "ms_mode=secure"
+	}
+
+	// If Encryption is not enabled, but Compression or RequireMsgr2 is enabled, use prefer-crc mode
+	if sc.Spec.Network != nil && sc.Spec.Network.Connections != nil &&
+		((sc.Spec.Network.Connections.Compression != nil && sc.Spec.Network.Connections.Compression.Enabled) ||
+			sc.Spec.Network.Connections.RequireMsgr2) {
+		return "ms_mode=prefer-crc"
+	}
+
+	// Network spec always has higher precedence even in the External or Provider cluster. so they are checked first above
+
+	// None of Encryption, Compression, RequireMsgr2 are enabled on the StorageCluster
+	// If it's an External or Provider cluster, We don't require msgr2 by default so no mount options are needed
 	if sc.Spec.ExternalStorage.Enable || sc.Spec.AllowRemoteStorageConsumers {
 		return ""
 	}
-	if sc.Spec.Network != nil && sc.Spec.Network.Connections != nil && sc.Spec.Network.Connections.Encryption != nil && sc.Spec.Network.Connections.Encryption.Enabled {
-		return "ms_mode=secure"
-	}
+	// If none of the above cases apply, We set RequireMsgr2 true by default on the cephcluster
+	// so we need to set the mount options to prefer-crc
 	return "ms_mode=prefer-crc"
 }
