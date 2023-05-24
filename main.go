3627129665fb7b782acb6776ca064c0dba6e5918
Nikhil-Ladha <nikhilladha1999@gmail.com>
2023-05-24 18:11:47 +0530

    Add namespace controller for ReclaimSpace
    Added a new namespace controller that adds a reclaimspace schedule
    annotation to all the namespaces starting with `openshift-*`, and
    restarts the csi-addons pod once done.
    
    **NOTE:**
    The controller doesn't restart the csi-addons pod if the value of
    reclaimspace annotation is updated to a different value manully.
    The pod needs to be restarted manually in that case.
    
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ import (
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	ocsv1 "github.com/red-hat-storage/ocs-operator/api/v1"
 	ocsv1alpha1 "github.com/red-hat-storage/ocs-operator/api/v1alpha1"
+	"github.com/red-hat-storage/ocs-operator/controllers/namespace"
 	"github.com/red-hat-storage/ocs-operator/controllers/ocsinitialization"
 	"github.com/red-hat-storage/ocs-operator/controllers/storageclassrequest"
 	"github.com/red-hat-storage/ocs-operator/controllers/storagecluster"
@@ -145,6 +146,15 @@ func main() {
 	condition, err := storagecluster.NewUpgradeable(mgr.GetClient())
 	if err != nil {
 		setupLog.Error(err, "Unable to get OperatorCondition")
+		os.Exit(1)
+	}
+
+	if err = (&namespace.NamespaceReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Namespace"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Namespace")
 		os.Exit(1)
 	}
 
