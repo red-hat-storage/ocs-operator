ea4ad1939ea177b81f0475fd9cc05062c72c792a
Malay Kumar Parida <mparida@redhat.com>
2023-06-22 08:54:07 +0000

    Add make targets with hack shell scripts to install ocs & noobaa
    Both the scripts use operator-sdk run bundle command to directly
    install the operators from their bundles. The scripts also wait for
    the operator deployments to be ready before exiting.
    
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

---
+++ hack/install-noobaa.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+source hack/common.sh
+
+NAMESPACE=$(oc get ns "$INSTALL_NAMESPACE" -o jsonpath="{.metadata.name}" 2>/dev/null || true)
+if [[ -n "$NAMESPACE" ]]; then
+    echo "Namespace \"$NAMESPACE\" exists"
+else
+    echo "Namespace \"$INSTALL_NAMESPACE\" does not exist: creating it"
+    oc create ns "$INSTALL_NAMESPACE"
+fi
+
+operator-sdk run bundle "$NOOBAA_BUNDLE_FULL_IMAGE_NAME" --timeout=10m --security-context-config restricted -n "$INSTALL_NAMESPACE"
+
+oc wait --timeout=5m --for condition=Available -n "$INSTALL_NAMESPACE" deployment noobaa-operator
---
+++ hack/install-ocs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+source hack/common.sh
+
+NAMESPACE=$(oc get ns "$INSTALL_NAMESPACE" -o jsonpath="{.metadata.name}" 2>/dev/null || true)
+if [[ -n "$NAMESPACE" ]]; then
+    echo "Namespace \"$NAMESPACE\" exists"
+else
+    echo "Namespace \"$INSTALL_NAMESPACE\" does not exist: creating it"
+    oc create ns "$INSTALL_NAMESPACE"
+fi
+
+operator-sdk run bundle "$BUNDLE_FULL_IMAGE_NAME" --timeout=10m --security-context-config restricted -n "$INSTALL_NAMESPACE"
+
+oc wait --timeout=5m --for condition=Available -n "$INSTALL_NAMESPACE" deployment ocs-operator
+oc wait --timeout=5m --for condition=Available -n "$INSTALL_NAMESPACE" deployment ocs-metrics-exporter
+oc wait --timeout=5m --for condition=Available -n "$INSTALL_NAMESPACE" deployment rook-ceph-operator
