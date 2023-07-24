baa14ad25981794256474e377d8efb5c7b6cc6e0
Malay Kumar Parida <mparida@redhat.com>
2023-07-24 11:14:29 +0000

    Use klog v2 instead of v1
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- metrics/internal/collectors/ceph-blocklist.go
+++ metrics/internal/collectors/ceph-blocklist.go
@@ -10,7 +10,7 @@ import (
 	"golang.org/x/net/context"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clientset "k8s.io/client-go/kubernetes"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 )
 
 var _ prometheus.Collector = &CephBlocklistCollector{}
