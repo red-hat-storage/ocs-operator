304e7612089246406d3e10ba08ce7ca22604cc2b
Nikhil-Ladha <nikhilladha1999@gmail.com>
2023-08-25 15:20:58 +0530

    Add annotation for multus in toolbox after checking the multus network name for namespace
    It is possible that the NetworkAttachmentDefinition for multus network
    is configured in a different namespace than the one in which the operator is deployed
    and multus is configured. In that case, the selector (eg: public) value in the
    storagecluster CR is of the following pattern `NAD_namespace/NAD_name`.
    
    Therefore, check for namespace value in the selector and add annotation to the
    ceph toolbox accordingly.
    
    BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1982721
    
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>

--- controllers/storagecluster/cephtoolbox.go
+++ controllers/storagecluster/cephtoolbox.go
@@ -3,15 +3,20 @@ package storagecluster
 import (
 	"context"
 	"fmt"
+	"os"
 	"reflect"
+	"strings"
 
+	nadclientset "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/clientset/versioned"
 	ocsv1 "github.com/red-hat-storage/ocs-operator/v4/api/v1"
 	"github.com/red-hat-storage/ocs-operator/v4/controllers/defaults"
 	"github.com/red-hat-storage/ocs-operator/v4/controllers/util"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
@@ -123,15 +128,41 @@ func getMultusPublicNetwork(sc *ocsv1.StorageCluster) (string, error) {
 		return "", err
 	}
 
-	multusNetNs, err := util.GetOperatorNamespace()
+	if sc.Spec.Network.Selectors["public"] == "" {
+		return "", nil
+	}
+
+	multusNetName := sc.Spec.Network.Selectors["public"]
+	multusNetNamespacedName := strings.Split(multusNetName, "/")
+	nadNS, nadName := "", ""
+	if len(multusNetNamespacedName) == 1 {
+		nadNS, nadName = os.Getenv(util.OperatorNamespaceEnvVar), multusNetName
+	} else if len(multusNetNamespacedName) == 2 {
+		nadNS, nadName = multusNetNamespacedName[0], multusNetNamespacedName[1]
+	} else {
+		return "", fmt.Errorf("Spec.Network.Selectors[\"public\"] value: %s in storagecluster CR is invalid", multusNetName)
+	}
+
+	nadClient, err := getNADClient()
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to get NAD client. %v", err)
+	}
+	_, err = nadClient.K8sCniCncfIoV1().NetworkAttachmentDefinitions(nadNS).Get(context.TODO(), nadName, metav1.GetOptions{})
+	if err != nil {
+		return "", fmt.Errorf("Either create the NetworkAttachmentDefinition or alter the storagecluster Spec.Network.Selectors[\"public\"] with correct value. %v", err)
 	}
 
-	if sc.Spec.Network.Selectors["public"] != "" {
-		multusNetName := sc.Spec.Network.Selectors["public"]
-		return fmt.Sprintf("%s/%s", multusNetNs, multusNetName), nil
-	}
+	return fmt.Sprintf("%s/%s", nadNS, nadName), nil
+}
 
-	return "", nil
+func getNADClient() (*nadclientset.Clientset, error) {
+	cfg, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		return nil, fmt.Errorf("failed to build config for NAD. %v", err)
+	}
+	client, err := nadclientset.NewForConfig(cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create a new Clientset for NAD. %v", err)
+	}
+	return client, nil
 }
