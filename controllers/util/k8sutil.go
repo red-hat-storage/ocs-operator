6477a7020fecb7c8cb65ae3312d0efa1f96f575c
Malay Kumar Parida <mparida@redhat.com>
2023-09-05 09:56:56 +0000

    Refactor ocs-operator-config related code for easier maintainability
    The keys for the configmap are defined duplicately in 2 places. So
    keeping these keys in a single place will make it easier to maintain
    them. Also some util functions which might be used by others are
    moved to the util package.
    
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- controllers/util/k8sutil.go
+++ controllers/util/k8sutil.go
@@ -1,9 +1,16 @@
 package util
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/go-logr/logr"
+	configv1 "github.com/openshift/api/config/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 const (
@@ -20,6 +27,14 @@ const (
 
 	// This configmap is watched by rook-ceph-operator & is reserved only for manual overrides.
 	RookCephOperatorConfigName = "rook-ceph-operator-config"
+
+	// These are the keys in the ocs-operator-config configmap
+	ClusterNameKey              = "CSI_CLUSTER_NAME"
+	EnableReadAffinityKey       = "CSI_ENABLE_READ_AFFINITY"
+	CephFSKernelMountOptionsKey = "CSI_CEPHFS_KERNEL_MOUNT_OPTIONS"
+	EnableTopologyKey           = "CSI_ENABLE_TOPOLOGY"
+	TopologyDomainLabelsKey     = "CSI_TOPOLOGY_DOMAIN_LABELS"
+	EnableNFSKey                = "ROOK_CSI_ENABLE_NFS"
 )
 
 // GetWatchNamespace returns the namespace the operator should be watching for changes
@@ -48,4 +63,34 @@ func GetOperatorNamespace() (string, error) {
 func IsSingleNodeDeployment() bool {
 	isSingleNode := os.Getenv(SingleNodeEnvVar)
 	return strings.ToLower(strings.TrimSpace(isSingleNode)) == "true"
+}
+
+// getClusterID returns the cluster ID of the OCP-Cluster
+func GetClusterID(ctx context.Context, kubeClient client.Client, logger *logr.Logger) string {
+	clusterVersion := &configv1.ClusterVersion{}
+	err := kubeClient.Get(ctx, types.NamespacedName{Name: "version"}, clusterVersion)
+	if err != nil {
+		logger.Error(err, "Failed to get the clusterVersion version of the OCP cluster")
+		return ""
+	}
+	return fmt.Sprint(clusterVersion.Spec.ClusterID)
+}
+
+// RestartPod restarts the pod with the given name in the given namespace by deleting it and letting another one be created
+func RestartPod(ctx context.Context, kubeClient client.Client, logger *logr.Logger, name string, namespace string) {
+	logger.Info("restarting pod", "name", name, "namespace", namespace)
+	podList := &corev1.PodList{}
+	err := kubeClient.List(ctx, podList, client.InNamespace(namespace))
+	if err != nil {
+		logger.Error(err, "failed to list pods", "namespace", namespace)
+		return
+	}
+	for _, pod := range podList.Items {
+		if strings.Contains(pod.Name, name) {
+			err = kubeClient.Delete(ctx, &pod)
+			if err != nil {
+				logger.Error(err, "failed to delete pod", "name", pod.Name, "namespace", namespace)
+			}
+		}
+	}
 }
