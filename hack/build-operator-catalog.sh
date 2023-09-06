da57554fd05e77450b7777617e0f6cfb6ccd92f7
Malay Kumar Parida <mparida@redhat.com>
2023-09-06 12:25:16 +0000

    Specify --platform=OS/ARCH where required in docker/podman build/run
    This enables us to make ocs-operator development on different
    os/arch platforms.
    
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- hack/build-operator-catalog.sh
+++ hack/build-operator-catalog.sh
@@ -16,6 +16,6 @@ ${OPM} validate catalog
 ${OPM} generate dockerfile catalog
 
 mv catalog.Dockerfile Dockerfile.catalog
-${IMAGE_BUILD_CMD} build --no-cache -t "${FILE_BASED_CATALOG_FULL_IMAGE_NAME}" -f Dockerfile.catalog .
+${IMAGE_BUILD_CMD} build --platform="${GOOS}"/"${GOARCH}" --no-cache -t "${FILE_BASED_CATALOG_FULL_IMAGE_NAME}" -f Dockerfile.catalog .
 
 echo "Run '${IMAGE_BUILD_CMD} push ${FILE_BASED_CATALOG_FULL_IMAGE_NAME}' to push operator catalog image to image registry."
