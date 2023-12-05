844d5227f88f63427865e63edaa5a8e7961ffd88
Umanga Chapagain <chapagainumanga@gmail.com>
2023-12-05 13:47:25 +0530

    add RDR optimization status to ClusterClaim
    Regional DR optimization status is crucial in configuring clusters
    for Regional DR. It is set to true when all optimizations are applied.
    
    Signed-off-by: Umanga Chapagain <chapagainumanga@gmail.com>

--- controllers/storagecluster/clusterclaims.go
+++ controllers/storagecluster/clusterclaims.go
@@ -9,12 +9,14 @@ import (
 	"github.com/go-logr/logr"
 	operatorsv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
 	ocsv1 "github.com/red-hat-storage/ocs-operator/v4/api/v1"
+	rookCephv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
 	corev1 "k8s.io/api/core/v1"
 	extensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/version"
 	"k8s.io/client-go/tools/clientcmd"
 	clusterclientv1alpha1 "open-cluster-management.io/api/client/cluster/clientset/versioned"
 	clusterv1alpha1 "open-cluster-management.io/api/cluster/v1alpha1"
@@ -30,12 +32,13 @@ const (
 )
 
 var (
-	ClusterClaimGroup   = "odf"
-	OdfVersion          = fmt.Sprintf("version.%s.openshift.io", ClusterClaimGroup)
-	StorageSystemName   = fmt.Sprintf("storagesystemname.%s.openshift.io", ClusterClaimGroup)
-	StorageClusterName  = fmt.Sprintf("storageclustername.%s.openshift.io", ClusterClaimGroup)
-	StorageClusterCount = fmt.Sprintf("count.storageclusters.%s.openshift.io", ClusterClaimGroup)
-	CephFsid            = fmt.Sprintf("cephfsid.%s.openshift.io", ClusterClaimGroup)
+	ClusterClaimGroup         = "odf"
+	OdfVersion                = fmt.Sprintf("version.%s.openshift.io", ClusterClaimGroup)
+	StorageSystemName         = fmt.Sprintf("storagesystemname.%s.openshift.io", ClusterClaimGroup)
+	StorageClusterName        = fmt.Sprintf("storageclustername.%s.openshift.io", ClusterClaimGroup)
+	StorageClusterCount       = fmt.Sprintf("count.storageclusters.%s.openshift.io", ClusterClaimGroup)
+	StorageClusterDROptimized = fmt.Sprintf("droptimized.%s.openshift.io", ClusterClaimGroup)
+	CephFsid                  = fmt.Sprintf("cephfsid.%s.openshift.io", ClusterClaimGroup)
 )
 
 type ocsClusterClaim struct{}
@@ -100,11 +103,18 @@ func (obj *ocsClusterClaim) ensureCreated(r *StorageClusterReconciler, instance 
 		return reconcile.Result{}, err
 	}
 
+	isDROptimized, err := creator.getIsDROptimized(r.serverVersion)
+	if err != nil {
+		r.Log.Error(err, "failed to get cephcluster status. retrying again")
+		return reconcile.Result{}, err
+	}
+
 	err = creator.setStorageClusterCount(strconv.Itoa(storageClusterCount)).
 		setStorageSystemName(storageSystemName).
 		setStorageClusterName(instance.Name).
 		setOdfVersion(odfVersion).
 		setCephFsid(cephFsid).
+		setDROptimized(isDROptimized).
 		create()
 
 	return reconcile.Result{}, err
@@ -193,6 +203,26 @@ func (c *ClusterClaimCreator) getCephFsid() (string, error) {
 	return "", fmt.Errorf("failed to fetch ceph fsid from %q secret", RookCephMonSecretName)
 }
 
+func (c *ClusterClaimCreator) getIsDROptimized(serverVersion *version.Info) (string, error) {
+	var cephCluster rookCephv1.CephCluster
+	err := c.Client.Get(c.Context, types.NamespacedName{Name: generateNameForCephClusterFromString(c.StorageCluster.Name), Namespace: c.StorageCluster.Namespace}, &cephCluster)
+	if err != nil {
+		return "false", err
+	}
+	if cephCluster.Status.CephStorage == nil || cephCluster.Status.CephStorage.OSD.StoreType == nil {
+		return "false", fmt.Errorf("cephcluster status does not have OSD store information")
+	}
+	bluestorerdr, ok := cephCluster.Status.CephStorage.OSD.StoreType["bluestore-rdr"]
+	if !ok {
+		return "false", nil
+	}
+	total := getOsdCount(c.StorageCluster, serverVersion)
+	if bluestorerdr < total {
+		return "false", nil
+	}
+	return "true", nil
+}
+
 func (c *ClusterClaimCreator) setStorageClusterCount(count string) *ClusterClaimCreator {
 	c.Values[StorageClusterCount] = count
 	return c
@@ -215,6 +245,11 @@ func (c *ClusterClaimCreator) setStorageClusterName(name string) *ClusterClaimCr
 
 func (c *ClusterClaimCreator) setCephFsid(fsid string) *ClusterClaimCreator {
 	c.Values[CephFsid] = fsid
+	return c
+}
+
+func (c *ClusterClaimCreator) setDROptimized(optimized string) *ClusterClaimCreator {
+	c.Values[StorageClusterDROptimized] = optimized
 	return c
 }
 
