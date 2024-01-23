573a1aa94e2a1983502cad908a3efa6b1d701f1f
Rewant Soni <resoni@redhat.com>
2024-01-23 11:42:15 +0530

    Add new CR and controller for storageClusterPeer
    ran the command:
    operator-sdk create api --group ocs --version v1
    --kind StorageClusterPeer --resource --controller
    
    Signed-off-by: Rewant Soni <resoni@redhat.com>

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ import (
 	"github.com/red-hat-storage/ocs-operator/v4/controllers/platform"
 	"github.com/red-hat-storage/ocs-operator/v4/controllers/storageclassrequest"
 	"github.com/red-hat-storage/ocs-operator/v4/controllers/storagecluster"
+	"github.com/red-hat-storage/ocs-operator/v4/controllers/storageclusterpeer"
 	controllers "github.com/red-hat-storage/ocs-operator/v4/controllers/storageconsumer"
 	"github.com/red-hat-storage/ocs-operator/v4/controllers/util"
 	cephv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
@@ -64,6 +65,7 @@ import (
 	apiclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metrics "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
 	// +kubebuilder:scaffold:imports
 )
 
@@ -215,6 +217,13 @@ func main() {
 		OperatorNamespace: operatorNamespace,
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "StorageClassRequest")
+		os.Exit(1)
+	}
+	if err = (&storageclusterpeer.StorageClusterPeerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "StorageClusterPeer")
 		os.Exit(1)
 	}
 	// +kubebuilder:scaffold:builder
