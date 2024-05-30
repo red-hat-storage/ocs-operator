3225047c8dcb9837db2c77ff3c5a8cdcb2be7eab
Nikhil-Ladha <nikhilladha1999@gmail.com>
2024-05-30 21:11:34 +0530

    Remove default value of waitTimeoutForHealthyOSDInMinutes
    Remove default value of waitTimeoutForHealthyOSDInMinutes
    from ocs-operator, and only update the SC CR, if a value is
    provided by the user.
    
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>

--- controllers/storagecluster/cephcluster.go
+++ controllers/storagecluster/cephcluster.go
@@ -3,7 +3,6 @@ package storagecluster
 import (
 	// The embed package is required for the prometheus rule files
 	_ "embed"
-	"time"
 
 	"bytes"
 	"context"
@@ -428,6 +427,10 @@ func newCephCluster(sc *ocsv1.StorageCluster, cephImage string, nodeCount int, s
 		},
 	}
 
+	if sc.Spec.ManagedResources.CephCluster.WaitTimeoutForHealthyOSDInMinutes != 0 {
+		cephCluster.Spec.WaitTimeoutForHealthyOSDInMinutes = sc.Spec.ManagedResources.CephCluster.WaitTimeoutForHealthyOSDInMinutes
+	}
+
 	if sc.Spec.LogCollector != nil {
 		if sc.Spec.LogCollector.Periodicity != "" {
 			cephCluster.Spec.LogCollector.Periodicity = sc.Spec.LogCollector.Periodicity
@@ -570,7 +573,6 @@ func newExternalCephCluster(sc *ocsv1.StorageCluster, cephImage, monitoringIP, m
 			Labels: rookCephv1.LabelsSpec{
 				rookCephv1.KeyMonitoring: getCephClusterMonitoringLabels(*sc),
 			},
-			WaitTimeoutForHealthyOSDInMinutes: getWaitTimeoutForHealthOSD(sc),
 		},
 	}
 
@@ -1183,12 +1185,4 @@ func getMonitoringClient() (*monitoringclient.Clientset, error) {
 		return nil, fmt.Errorf("failed to get monitoring client bar. %v", err)
 	}
 	return client, nil
-}
-
-func getWaitTimeoutForHealthOSD(sc *ocsv1.StorageCluster) time.Duration {
-	if sc.Spec.ManagedResources.CephCluster.WaitTimeoutForHealthyOSDInMinutes != 0 {
-		return sc.Spec.ManagedResources.CephCluster.WaitTimeoutForHealthyOSDInMinutes
-	}
-
-	return defaults.DefaultWaitTimeoutForHealthyOSD
 }
