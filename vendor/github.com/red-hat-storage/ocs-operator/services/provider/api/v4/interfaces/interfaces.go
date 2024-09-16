0c895db48edd7c5f9cbc9b6d4637446208c644bc
Leela Venkaiah G <lgangava@ibm.com>
2024-09-16 11:51:07 +0000

    go-deps: vendor changes for newly created modules
    Signed-off-by: Leela Venkaiah G <lgangava@ibm.com>

---
+++ vendor/github.com/red-hat-storage/ocs-operator/services/provider/api/v4/interfaces/interfaces.go
@@ -0,0 +1,29 @@
+package interfaces
+
+type StorageClientStatus interface {
+	// TODO: it was mistake not using full name of the field and we are just
+	// doing indirection for getters, change this interface after ensuring
+	// no client is using it
+	GetPlatformVersion() string
+	GetOperatorVersion() string
+	GetClusterID() string
+	GetClusterName() string
+	GetClientName() string
+
+	SetPlatformVersion(string) StorageClientStatus
+	SetOperatorVersion(string) StorageClientStatus
+	SetClusterID(string) StorageClientStatus
+	SetClusterName(string) StorageClientStatus
+	SetClientName(string) StorageClientStatus
+}
+
+type StorageClientOnboarding interface {
+	// getters for fields are already provided by protobuf messages
+	GetOnboardingTicket() string
+	GetConsumerName() string
+	GetClientOperatorVersion() string
+
+	SetOnboardingTicket(string) StorageClientOnboarding
+	SetConsumerName(string) StorageClientOnboarding
+	SetClientOperatorVersion(string) StorageClientOnboarding
+}
