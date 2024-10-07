89880ff97d9b21bbd96d72b21f0b3fbc950c1c91
Rewant Soni <resoni@redhat.com>
2024-10-07 19:29:41 +0530

    onboardingTicket: add new uid to the ticket
    onboarding ticket should contain the storageCluster UID the token
    was generated for and for that it requires input for the
    namespacedName of the storageCluster we want to generate the
    ticket for
    
    Signed-off-by: Rewant Soni <resoni@redhat.com>

--- controllers/util/k8sutil.go
+++ controllers/util/k8sutil.go
@@ -166,3 +166,15 @@ func GenerateNameForNonResilientCephBlockPoolSC(initData *ocsv1.StorageCluster) 
 	}
 	return fmt.Sprintf("%s-ceph-non-resilient-rbd", initData.Name)
 }
+
+func GetStorageClusterInNamespace(ctx context.Context, cl client.Client, storageClusterNamespace string) (*ocsv1.StorageCluster, error) {
+	storageClusterList := &ocsv1.StorageClusterList{}
+	err := cl.List(ctx, storageClusterList, client.InNamespace(storageClusterNamespace))
+	if err != nil {
+		return nil, fmt.Errorf("failed to list StorageCluster in namespace %s: %v", storageClusterNamespace, err)
+	}
+	if len(storageClusterList.Items) != 1 {
+		return nil, fmt.Errorf("expected 1 StorageCluster in namespace but got %v", len(storageClusterList.Items))
+	}
+	return &storageClusterList.Items[0], nil
+}
