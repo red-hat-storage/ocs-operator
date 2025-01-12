081fb53adbe3b0ef67890533c0b8178813e23d2f
Oded Viner <oviner@redhat.com>
2025-01-12 14:51:21 +0200

    Restrict Node Label Matching for Supported Topology Keys
    Restrict topology key matching to explicitly supported labels
    to prevent incorrect updates and ensure reliable cluster
    configuration.
    
    Signed-off-by: Oded Viner <oviner@redhat.com>

--- controllers/storagecluster/placement_test.go
+++ controllers/storagecluster/placement_test.go
@@ -377,7 +377,7 @@ func TestGetPlacement(t *testing.T) {
 											},
 										},
 									},
-									TopologyKey: zoneTopologyLabel,
+									TopologyKey: corev1.LabelZoneFailureDomainStable,
 								},
 							},
 						},
@@ -397,7 +397,7 @@ func TestGetPlacement(t *testing.T) {
 										},
 									},
 								},
-								TopologyKey: zoneTopologyLabel,
+								TopologyKey: corev1.LabelZoneFailureDomainStable,
 							},
 						},
 					},
@@ -405,7 +405,7 @@ func TestGetPlacement(t *testing.T) {
 			},
 			topologyMap: &ocsv1.NodeTopologyMap{
 				Labels: map[string]ocsv1.TopologyLabelValues{
-					zoneTopologyLabel: []string{
+					corev1.LabelZoneFailureDomainStable: []string{
 						"zone1",
 						"zone2",
 						"zone3",
@@ -490,7 +490,9 @@ func TestGetPlacement(t *testing.T) {
 		sc.Spec.Placement = c.placements
 		sc.Spec.LabelSelector = c.labelSelector
 		sc.Status.NodeTopologies = c.topologyMap
-
+		if c.topologyMap != nil {
+			setFailureDomain(sc)
+		}
 		expectedPlacement := c.expectedPlacements["all"]
 		actualPlacement = getPlacement(sc, "all")
 		assert.Equal(t, expectedPlacement, actualPlacement, c.label)
