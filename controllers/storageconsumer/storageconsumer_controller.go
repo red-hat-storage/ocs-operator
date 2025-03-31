0f3963536ce667733a67b06b5fc2370f0d32ede8
Leela Venkaiah G <lgangava@ibm.com>
2025-03-31 22:42:10 +0530

    check consumer status for clusterid before reconciling noobaaaccount
    after consumer is enabled, client doesn't report status instantaneously
    and if it's empty we reconcileNoobaaAccount once, which'll get into a
    deadlock as noobaa will not be ready until we send storageconfig for
    storageclass creation which will check for consumer readiness and it'll
    never be ready.
    
    Signed-off-by: Leela Venkaiah G <lgangava@ibm.com>

--- controllers/storageconsumer/storageconsumer_controller.go
+++ controllers/storageconsumer/storageconsumer_controller.go
@@ -302,7 +302,10 @@ func (r *StorageConsumerReconciler) reconcileEnabledPhases() (reconcile.Result, 
 		// to connect to a remote cluster, unlike client clusters.
 		// A NooBaa account only needs to be created if the storage consumer is for a client cluster.
 		clusterID := util.GetClusterID(r.ctx, r.Client, &r.Log)
-		if clusterID != "" && r.storageConsumer.Status.Client.ClusterID != clusterID {
+		clientStatus := &r.storageConsumer.Status.Client
+		if clusterID != "" &&
+			clientStatus.ClusterID != "" &&
+			clientStatus.ClusterID != clusterID {
 			if err := r.reconcileNoobaaAccount(); err != nil {
 				return reconcile.Result{}, err
 			}
