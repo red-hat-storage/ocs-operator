ec37a4f379c0aa71933d7257d3bf264c14471da7
Leela Venkaiah G <lgangava@ibm.com>
2025-04-10 13:52:26 +0530

    add support for distributing day2 volume[group]snapshotclasses
    when the volume[group]storageclass mentioned in storageconsumer
    doesn't match one of the known names we consider that being unmanaged
    day2 created v[g]sc and we update the dynamic params corresponding to
    the driver and then send as a resource to be created & owned by
    storageclient in client-op.
    
    Signed-off-by: Leela Venkaiah G <lgangava@ibm.com>

--- metrics/vendor/github.com/red-hat-storage/ocs-operator/v4/controllers/util/snapshotclasses.go
+++ metrics/vendor/github.com/red-hat-storage/ocs-operator/v4/controllers/util/snapshotclasses.go
@@ -1,9 +1,14 @@
 package util
 
 import (
+	"context"
+	"errors"
 	"fmt"
+
 	snapapi "github.com/kubernetes-csi/external-snapshotter/client/v8/apis/volumesnapshot/v1"
+	ocsv1a1 "github.com/red-hat-storage/ocs-operator/api/v4/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // SnapshotterType represents a snapshotter type
@@ -13,6 +18,10 @@ const (
 	RbdSnapshotter    SnapshotterType = "rbd"
 	CephfsSnapshotter SnapshotterType = "cephfs"
 	NfsSnapshotter    SnapshotterType = "nfs"
+)
+
+var (
+	UnsupportedDriver = errors.New("unsupportedDriver")
 )
 
 // GenerateNameForSnapshotClass function generates 'SnapshotClass' name.
@@ -93,4 +102,52 @@ func NewDefaultNfsSnapshotClass(
 		DeletionPolicy: snapapi.VolumeSnapshotContentDelete,
 	}
 	return sc
+}
+
+func VolumeSnapshotClassFromExisting(
+	ctx context.Context,
+	kubeClient client.Client,
+	volumeSnapshotClassName string,
+	consumer *ocsv1a1.StorageConsumer,
+	consumerConfig StorageConsumerResources,
+	rbdStorageId,
+	cephFsStorageId,
+	nfsStorageId string,
+) (*snapapi.VolumeSnapshotClass, error) {
+	snapshotClass := &snapapi.VolumeSnapshotClass{}
+	snapshotClass.Name = volumeSnapshotClassName
+	if err := kubeClient.Get(ctx, client.ObjectKeyFromObject(snapshotClass), snapshotClass); err != nil {
+		return nil, err
+	}
+	clientProfileName := ""
+	provisionerSecretName := ""
+	storageId := ""
+	operatorNamespace := consumer.Status.Client.OperatorNamespace
+	switch snapshotClass.Driver {
+	case RbdDriverName:
+		clientProfileName = consumerConfig.GetRbdClientProfileName()
+		provisionerSecretName = consumerConfig.GetCsiRbdProvisionerSecretName()
+		storageId = rbdStorageId
+	case CephFSDriverName:
+		clientProfileName = consumerConfig.GetCephFsClientProfileName()
+		provisionerSecretName = consumerConfig.GetCsiCephFsProvisionerSecretName()
+		storageId = cephFsStorageId
+	case NfsDriverName:
+		clientProfileName = consumerConfig.GetNfsClientProfileName()
+		provisionerSecretName = consumerConfig.GetCsiNfsProvisionerSecretName()
+		storageId = nfsStorageId
+	default:
+		return nil, UnsupportedDriver
+	}
+
+	params := snapshotClass.Parameters
+	if params == nil {
+		params = map[string]string{}
+		snapshotClass.Parameters = params
+	}
+	params["clusterID"] = clientProfileName
+	params["csi.storage.k8s.io/snapshotter-secret-name"] = provisionerSecretName
+	params["csi.storage.k8s.io/snapshotter-secret-namespace"] = operatorNamespace
+	AddLabel(snapshotClass, storageIdLabelKey, storageId)
+	return snapshotClass, nil
 }
