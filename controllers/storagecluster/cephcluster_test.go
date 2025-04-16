a93364d191402f5cd4c2f1696406dc68f093566d
Malay Kumar Parida <mparida@redhat.com>
2025-04-16 14:50:14 +0530

    Add unit test for the target pg_per_osd behavior in different scenarios
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- controllers/storagecluster/cephcluster_test.go
+++ controllers/storagecluster/cephcluster_test.go
@@ -9,13 +9,16 @@ import (
 	"testing"
 	"time"
 
+	"github.com/imdario/mergo"
 	nbv1 "github.com/noobaa/noobaa-operator/v5/pkg/apis/noobaa/v1alpha1"
 	configv1 "github.com/openshift/api/config/v1"
 	conditionsv1 "github.com/openshift/custom-resource-status/conditions/v1"
+	api "github.com/red-hat-storage/ocs-operator/api/v4/v1"
 	ocsv1 "github.com/red-hat-storage/ocs-operator/api/v4/v1"
 	"github.com/red-hat-storage/ocs-operator/v4/controllers/defaults"
 	"github.com/red-hat-storage/ocs-operator/v4/controllers/platform"
 	ocsutil "github.com/red-hat-storage/ocs-operator/v4/controllers/util"
+	cephv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
 	rookCephv1 "github.com/rook/rook/pkg/apis/ceph.rook.io/v1"
 	"gotest.tools/v3/assert"
 	is "gotest.tools/v3/assert/cmp"
@@ -90,6 +93,7 @@ func TestEnsureCephCluster(t *testing.T) {
 		reconciler := createFakeStorageClusterReconciler(t, networkConfig)
 
 		expected := newCephCluster(mockStorageCluster.DeepCopy(), "", nil, log)
+		setMonTargetPgPerOsd(&expected.Spec.CephConfig)
 		expected.Status.State = c.cephClusterState
 
 		if !c.shouldCreate {
@@ -1757,4 +1761,120 @@ func TestIsEncrptionSettingUpdated(t *testing.T) {
 	// encryption setting has not changed
 	actualResult = isEncrptionSettingUpdated(false, newStorageClassDeviceSet)
 	assert.Equal(t, false, actualResult)
+}
+
+func TestMonTargetPgPerOsdBehaviour(t *testing.T) {
+	var cases = []struct {
+		description            string
+		existing               *cephv1.CephCluster
+		storageClusterSpec     *api.StorageClusterSpec
+		expectedTargetPGPerOsd string
+	}{
+		{
+			description:            "case 1: New cluster creation - TargetPGPerOsd should be set to 400",
+			expectedTargetPGPerOsd: "400",
+		},
+		{
+			description: "case 2: New pool creation, TargetPGPerOsd is specified on CR- should respect CR setting",
+			storageClusterSpec: &api.StorageClusterSpec{
+				ManagedResources: api.ManagedResourcesSpec{
+					CephCluster: api.ManageCephCluster{
+						CephConfig: map[string]map[string]string{
+							"global": {
+								"mon_target_pg_per_osd": "500",
+							},
+						},
+					},
+				},
+			},
+			expectedTargetPGPerOsd: "500",
+		},
+		{
+			description: "case 3: Existing cluster with target pg value - should preserve value",
+			existing: &cephv1.CephCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "cephcluster",
+				},
+				Spec: cephv1.ClusterSpec{
+					CephConfig: map[string]map[string]string{
+						"global": {
+							"mon_target_pg_per_osd": "400",
+						},
+					},
+				},
+			},
+			expectedTargetPGPerOsd: "400",
+		},
+		{
+			description: "case 4: Existing cluster without target pg value - should not set flag",
+			existing: &cephv1.CephCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "cephcluster",
+				},
+				Spec: cephv1.ClusterSpec{},
+			},
+			expectedTargetPGPerOsd: "",
+		},
+		{
+			description: "case 5: Existing cluster without target pg value - CR specifies target pg value - should respect CR setting",
+			existing: &cephv1.CephCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "cephcluster",
+				},
+				Spec: cephv1.ClusterSpec{},
+			},
+			storageClusterSpec: &api.StorageClusterSpec{
+				ManagedResources: api.ManagedResourcesSpec{
+					CephCluster: api.ManageCephCluster{
+						CephConfig: map[string]map[string]string{
+							"global": {
+								"mon_target_pg_per_osd": "200",
+							},
+						},
+					},
+				},
+			},
+			expectedTargetPGPerOsd: "200",
+		},
+	}
+
+	for _, c := range cases {
+		t.Logf("Running %s", c.description)
+		sc := &ocsv1.StorageCluster{}
+		mockStorageCluster.DeepCopyInto(sc)
+		sc.Status.Images.Ceph = &ocsv1.ComponentImageStatus{}
+		reconciler := createFakeStorageClusterReconciler(t, networkConfig)
+		expected := newCephCluster(mockStorageCluster.DeepCopy(), "", nil, log)
+		if c.storageClusterSpec != nil {
+			_ = mergo.Merge(&sc.Spec, c.storageClusterSpec)
+		}
+		err := reconciler.Client.Create(context.TODO(), sc)
+		assert.NilError(t, err)
+
+		if c.existing != nil {
+			c.existing.ObjectMeta.Name = expected.Name
+			c.existing.ObjectMeta.Namespace = expected.Namespace
+			err := reconciler.Client.Create(context.TODO(), c.existing)
+			assert.NilError(t, err)
+		}
+
+		obj := &ocsCephCluster{}
+		_, err = obj.ensureCreated(&reconciler, sc)
+		assert.NilError(t, err)
+
+		actual := &cephv1.CephCluster{}
+		err = reconciler.Client.Get(context.TODO(), types.NamespacedName{Name: expected.Name, Namespace: expected.Namespace}, actual)
+		assert.NilError(t, err)
+		assert.Equal(t, expected.ObjectMeta.Name, actual.ObjectMeta.Name)
+		assert.Equal(t, expected.ObjectMeta.Namespace, actual.ObjectMeta.Namespace)
+
+		targetPg, exists := actual.Spec.CephConfig["global"]["mon_target_pg_per_osd"]
+		if c.expectedTargetPGPerOsd == "" {
+			assert.Equal(t, false, exists, "mon_target_pg_per_osd should not exist")
+
+		} else {
+			assert.Equal(t, true, exists, "mon_target_pg_per_osd should exist")
+			assert.Equal(t, c.expectedTargetPGPerOsd, targetPg, "mon_target_pg_per_osd value mismatch")
+		}
+	}
 }
