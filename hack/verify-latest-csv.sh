1bd45eb72d13a8b719cddc8bc0fa29e2cb136399
Malay Kumar Parida <mparida@redhat.com>
2025-07-01 14:32:29 +0530

    Modify verify-latest-csv script to ignore csv createdAt timestamp change
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- hack/verify-latest-csv.sh
+++ hack/verify-latest-csv.sh
@@ -14,11 +14,11 @@ done
 
 if [[ -n "${NOT_FOUND}" ]];then
 	echo "latest CSV has not been generated"
-        echo "Missing images: ${NOT_FOUND}"
+	echo "Missing images: ${NOT_FOUND}"
 	exit 1
 fi
 
-if [[ -n "$(git status --porcelain deploy/csv-templates deploy/ocs-operator config/crd/bases )" ]]; then
+if ! git diff --quiet --ignore-matching-lines=createdAt deploy/csv-templates deploy/ocs-operator config/crd/bases; then
 	git diff -u deploy/csv-templates deploy/ocs-operator config/crd/bases
 	echo "uncommitted CSV changes. run 'make gen-latest-csv' and commit results."
 	exit 1
