f2ff4b63caec3f520f0fd464ac3c3823cfc1a506
Malay Kumar Parida <mparida@redhat.com>
2025-08-12 00:29:36 +0530

    Collect ocs must-gather to artifact directory in CI env for persistence
    Currently the must-gather is collected to a local directory which is
    lost after the test run. With this change, the must-gather is collected
    to the artifact directory in CI env for persistence. Also remove the
    collection of ocp must-gather as all the relevant information is already
    inside the ocs must-gather.
    
    Signed-off-by: Malay Kumar Parida <mparida@redhat.com>

--- hack/dump-debug-info.sh
+++ hack/dump-debug-info.sh
@@ -6,10 +6,11 @@ set -e
 
 echo "dumping debug information"
 
+if [[ -n "${ARTIFACT_DIR:-}" ]]; then
+    OCS_MUST_GATHER_DIR="${ARTIFACT_DIR}/${OCS_MUST_GATHER_DIR}"
+fi
+
 echo "Running ocs-must-gather to ${OCS_MUST_GATHER_DIR}"
 mkdir -p "$OCS_MUST_GATHER_DIR"
 ${OCS_OC_PATH} adm must-gather --image "$LATEST_MUST_GATHER_IMAGE" --dest-dir "$OCS_MUST_GATHER_DIR"
-
-echo "Running ocp-must-gather to ${OCP_MUST_GATHER_DIR}"
-mkdir -p "$OCP_MUST_GATHER_DIR"
-${OCS_OC_PATH} --insecure-skip-tls-verify adm must-gather --dest-dir "$OCP_MUST_GATHER_DIR"
+echo "OCS must-gather completed successfully."
