faebd0ed51cabd9861d4db7e3b78f1197e0a9b12
yati1998 <ypadia@redhat.com>
2025-09-05 11:42:15 +0530

    remove kube-rbac-proxy container from deployment
    This commit removes the kube-rc-proxy container and related
    usage from the deployment as it has been deprecated.
    
    Signed-off-by: yati1998 <ypadia@redhat.com>

--- templates/prometheus.go
+++ templates/prometheus.go
@@ -1,9 +1,6 @@
 package templates
 
 import (
-	"fmt"
-	"os"
-
 	promv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/red-hat-storage/ocs-operator/v4/controllers/defaults"
 	corev1 "k8s.io/api/core/v1"
@@ -18,9 +15,7 @@ var ruleSelector = metav1.LabelSelector{
 }
 
 var (
-	KubeRBACProxyPortNumber              = 9339
-	PrometheusKubeRBACProxyConfigMapName = "prometheus-kube-rbac-proxy-config"
-	AlertManagerEndpointName             = "alertmanager-operated"
+	AlertManagerEndpointName = "alertmanager-operated"
 )
 
 var PrometheusSpecTemplate = promv1.PrometheusSpec{
@@ -29,55 +24,6 @@ var PrometheusSpecTemplate = promv1.PrometheusSpec{
 		ServiceMonitorSelector: &metav1.LabelSelector{},
 		ListenLocal:            true,
 		Resources:              defaults.MonitoringResources["prometheus"],
-		Containers: []corev1.Container{{
-			Name:  "kube-rbac-proxy",
-			Image: os.Getenv("KUBE_RBAC_PROXY_IMAGE"),
-			Args: []string{
-				fmt.Sprintf("--secure-listen-address=0.0.0.0:%d", KubeRBACProxyPortNumber),
-				"--upstream=http://127.0.0.1:9090/",
-				"--logtostderr=true",
-				"--v=10",
-				"--tls-cert-file=/etc/tls-secret/tls.crt",
-				"--tls-private-key-file=/etc/tls-secret/tls.key",
-				"--client-ca-file=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt",
-				"--config-file=/etc/kube-rbac-config/config-file.json",
-			},
-			Ports: []corev1.ContainerPort{{
-				Name:          "https",
-				ContainerPort: int32(KubeRBACProxyPortNumber),
-			}},
-			VolumeMounts: []corev1.VolumeMount{
-				{
-					Name:      "serving-cert",
-					MountPath: "/etc/tls-secret",
-				},
-				{
-					Name:      "kube-rbac-config",
-					MountPath: "/etc/kube-rbac-config",
-				},
-			},
-			Resources: defaults.MonitoringResources["kube-rbac-proxy"],
-		}},
-		Volumes: []corev1.Volume{
-			{
-				Name: "serving-cert",
-				VolumeSource: corev1.VolumeSource{
-					Secret: &corev1.SecretVolumeSource{
-						SecretName: "prometheus-serving-cert-secret",
-					},
-				},
-			},
-			{
-				Name: "kube-rbac-config",
-				VolumeSource: corev1.VolumeSource{
-					ConfigMap: &corev1.ConfigMapVolumeSource{
-						LocalObjectReference: corev1.LocalObjectReference{
-							Name: PrometheusKubeRBACProxyConfigMapName,
-						},
-					},
-				},
-			},
-		},
 		Tolerations: []corev1.Toleration{{
 			Key:      defaults.NodeTolerationKey,
 			Operator: corev1.TolerationOpEqual,
