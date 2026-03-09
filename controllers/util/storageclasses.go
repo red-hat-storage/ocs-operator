76231d3a87b0fd52491c54121574949992d4f76a
shirady <57721533+shirady@users.noreply.github.com>
2026-03-09 08:11:36 +0200

    rename driver to provisioner
    Signed-off-by: shirady <57721533+shirady@users.noreply.github.com>

--- controllers/util/storageclasses.go
+++ controllers/util/storageclasses.go
@@ -420,7 +420,7 @@ func StorageClassFromExisting(
 	replicationID := ""
 	groupReplicationID := ""
 	operatorNamespace := consumer.Status.Client.OperatorNamespace
-	noobaaStorageClassName := consumer.Namespace + NoobaaDriverNameSuffix
+	NoobaaProvisionerName := consumer.Namespace + NoobaaProvisionerNameSuffix
 	switch storageClass.Provisioner {
 	case RbdDriverName:
 		clientProfileName = consumerConfig.GetRbdClientProfileName()
@@ -449,7 +449,7 @@ func StorageClassFromExisting(
 		provisionerSecretName = consumerConfig.GetCsiNfsProvisionerCephUserName()
 		nodeSecretName = consumerConfig.GetCsiNfsNodeCephUserName()
 		storageId = nfsStorageId
-	case noobaaStorageClassName:
+	case NoobaaProvisionerName:
 		return storageClass, nil
 	default:
 		return nil, UnsupportedProvisioner
