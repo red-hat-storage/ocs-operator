60a22b46050af040215fe42012e701e32215c885
Niraj Yadav <niryadav@redhat.com>
2026-04-15 16:36:32 +0530

    util: disable reclaimspace for rbd virt storageclass
    This patch disables reclaimspace for virt storageclass
    to prevent sparsify operations from blacklisting the kRBD
    clients (VMs).
    
    Signed-off-by: Niraj Yadav <niryadav@redhat.com>

--- metrics/vendor/github.com/red-hat-storage/ocs-operator/v4/controllers/util/storageclasses.go
+++ metrics/vendor/github.com/red-hat-storage/ocs-operator/v4/controllers/util/storageclasses.go
@@ -134,7 +134,6 @@ func NewDefaultVirtRbdStorageClass(
 		ObjectMeta: metav1.ObjectMeta{
 			Annotations: map[string]string{
 				"description": "Provides RWO and RWX Block volumes suitable for Virtual Machine disks",
-				"reclaimspace.csiaddons.openshift.io/schedule": "@weekly",
 			},
 			Labels: map[string]string{},
 		},
