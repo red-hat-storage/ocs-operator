ef2d4065c1dd1422dc0f8265b4659f7505b46bae
Divyansh Kamboj <dkamboj@redhat.com>
2026-04-22 15:06:58 +0530

    alerts: remove duplicate PVC usage alerts from local ceph rules
    These alerts are now owned by ocs-client-operator's
    prometheus-pvc-rules. Removing the duplicate from
    prometheus-ceph-rules avoids two PrometheusRules defining
    the same alert at different thresholds on internal mode
    clusters.
    
    Signed-off-by: Divyansh Kamboj <dkamboj@redhat.com>

--- controllers/storagecluster/cephcluster_test.go
+++ controllers/storagecluster/cephcluster_test.go
@@ -1040,7 +1040,7 @@ func TestNewCephDaemonResources(t *testing.T) {
 func TestParsePrometheusRules(t *testing.T) {
 	prometheusRules, err := parsePrometheusRule(localPrometheusRules)
 	assert.NilError(t, err)
-	assert.Equal(t, 11, len(prometheusRules.Spec.Groups))
+	assert.Equal(t, 10, len(prometheusRules.Spec.Groups))
 
 	prometheusRules, err = parsePrometheusRule(externalPrometheusRules)
 	assert.NilError(t, err)
